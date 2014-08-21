dda56c4a57b2f190a9fae5d72f3ec5fdc506efa5
Boomer Rehfield <angrek@gmail.com>
2014-08-21 09:18:06 -0400

    adding populate script

---
+++ populate.py
@@ -0,0 +1,34 @@
+#!/home/wrehfiel/ENV/bin/python2.7
+#########################################################################
+#
+# Django server database population script - just random info for testing
+#
+# Boomer Rehfield - 8/7/2014
+#
+#########################################################################
+
+import os
+
+#these are need in django 1.7 and needed vs the django settings command
+import django
+from dashboard import settings
+django.setup()
+
+#starting with AIX servers first
+def populate():
+    add_server(name="d1nim", os="AIX")
+    add_server(name="u2jdpdb", os="AIX")
+    add_server(name="u3webdb", os="AIX")
+
+def add_server(name, ip_address, os, os_level):
+    s = Server.objects.get_or_create(name=name, ip_address=ip_address, os=os, os_level=os_level)[0]
+    return s
+
+
+
+#start execution
+if __name__ == '__main__':
+    print "Starting populations..."
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
+    from server.models import Server
+    populate()
