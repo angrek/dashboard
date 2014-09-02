667390dc2dbf2f0099be3573a531d24891cd7b3d
Boomer Rehfield <william.rehfield@wellcare.com>
2014-09-02 15:51:02 -0400

    added top save buttons to server and log admin pages

--- server/admin.py
+++ server/admin.py
@@ -7,6 +7,7 @@ from django.contrib.admin.models import LogEntry
 
 class ServerAdmin(admin.ModelAdmin):
     list_max_show_all = 500
+    save_on_top = True
     list_display = ['name', 'active','exception', 'modified', 'os', 'os_level', 'centrify', 'xcelys', 'ssl']
     list_filter = ['os', 'os_level', 'active', 'exception', 'centrify', 'xcelys', 'ssl']
     search_fields = ['name', 'os', 'os_level', 'centrify', 'xcelys', 'ssl']
@@ -20,6 +21,7 @@ class LogEntryAdmin(admin.ModelAdmin):
     list_display = ('action_time', 'user', 'content_type', 'object_repr', 'action_flag', 'change_message')
     list_filter = ('action_time', 'user_id', 'content_type_id', 'object_repr', 'action_flag', 'change_message')
     search_fields = ('action_time', 'user_id', 'content_type_id', 'object_repr', 'action_flag', 'change_message')
+    save_on_top = True
     fields = ('action_time', 'user_id', 'content_type_id', 'object_repr', 'action_flag', 'change_message')
     order = ('-action_time')
     def has_add_permissions(self, request):
