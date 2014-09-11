e9e231cbb21cac1bf8007bbbad49de6c822e79a4
Boomer Rehfield <angrek@gmail.com>
2014-09-11 12:15:59 -0400

    removed frame name from list_display

--- server/admin.py
+++ server/admin.py
@@ -8,7 +8,7 @@ from django.contrib.admin.models import LogEntry
 class AIXServerAdmin(admin.ModelAdmin):
     list_max_show_all = 500
     save_on_top = True
-    list_display = ['name', 'frame', 'active','exception', 'modified', 'os', 'os_level', 'centrify', 'xcelys', 'ssl']
+    list_display = ['name', 'active','exception', 'modified', 'os', 'os_level', 'centrify', 'xcelys', 'ssl']
     list_filter = ['os', 'os_level', 'active', 'exception', 'centrify', 'xcelys', 'ssl']
     search_fields = ['name', 'os', 'os_level', 'centrify', 'xcelys', 'ssl']
     readonly_fields = ['created', 'modified']
