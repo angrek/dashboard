1f23a115b3480e83dc4f4e0508ebea5d80d2314a
Boomer Rehfield <angrek@gmail.com>
2014-09-22 16:07:24 -0400

    added the power7 file and made some preliminary test commands to the HMC

---
+++ aix_power7_inventory.py
@@ -0,0 +1,169 @@
+#!/home/wrehfiel/ENV/bin/python2.7
+#########################################################################
+#
+# Django, AIX Power7 inventory scripts. This is for gathering the mem
+#   and cpu stats right now from the HMC and drop it into Django.
+#
+# Boomer Rehfield - 9/22/2014
+#
+#########################################################################
+
+import os
+from ssh import SSHClient
+from django.utils import timezone
+from django.contrib.admin.models import LogEntry
+
+#these are need in django 1.7 and needed vs the django settings command
+import django
+from dashboard import settings
+from server.models import AIXServer
+import ping_server
+import logging
+django.setup()
+
+logging.basicConfig( level=logging.INFO )
+
+def populate():
+
+    f = open("../../.ssh/p", "r")
+
+    #need rstrip to strip off the newline at the end
+    password = str(f.read().rstrip())
+    f.close()
+    #do I need a ping test for p1hmc?? lol
+    client = SSHClient()
+    client.load_system_host_keys()
+    
+    #we don't have ssh keys to p1hmc so this is going to prompt us for a password for now
+    try:
+        client.connect('p1hmc', username="wrehfiel", password=password)
+    except:
+        print 'SSH to p1hmc has failed!'
+
+    #Grab all of the frames on the HMC
+    stdin, stdout, stderr = client.exec_command('lssyscfg -r sys -F name')
+
+    frames = stdout.readlines()
+    for frame in frames:
+        #the output is throwing newlines at the end of the names for some reason
+        #hence the use of rstrip below
+        #print frame.rstrip()
+
+        client = SSHClient()
+        client.load_system_host_keys()
+
+        #we've already established a connect6ion, but should put in error checking
+        #FIXME
+        client.connect('p1hmc', username="wrehfiel", password=password)
+
+
+        #for each frame, let's get all of the HMC memory data
+        command = 'lshwres -r mem -m ' + frame.rstrip() + ' --level lpar'
+        print command
+
+        stdin, stdout, stderr = client.exec_command(command)
+        lpar_list = stdout.readlines()
+        #we'll close the connection after the next section
+
+        lpar_array = {}
+        for lpar in lpar_list:
+            lpar_dict = lpar.split(",")
+            for entry in lpar_dict:
+                #test for an empty value
+                if entry:
+                    a,b = entry.split('=')
+                    lpar_array[a] = b
+
+        for key, value in lpar_array.iteritems():
+            print key, "=>",value
+
+
+
+
+        #for each frame, let's get all of the HMC CPU data for it
+        command = 'lshwres -r proc -m ' + frame.rstrip() + ' --level lpar'
+        print command
+
+        sdtin, stdout, stderr = client.exec_command(command)
+        lpar_list = stdout.readlines()
+        client.close()
+
+        lpar_array = {}
+        for lpar in lpar_list:
+            print ">>>>>>>> " + lpar
+            print " "
+            lpar_dict = lpar.split(",")
+            for entry in lpar_dict:
+                #test for an empty value
+                if entry:
+                    a,b = entry.split('=')
+                    lpar_array[a] = b
+        
+        for name, possible_items in lpar_array.iteritems():
+            print name, "=>",possible_items 
+
+
+        #Ok, here's the break in logic from the others. We don't need to hit the servers.
+        #For each frame, run the lshwres command and that will spit out the data for
+        #all of its LPARs. For each row, split by comma, and then by equal sign and
+        #drop into an array.
+
+
+
+#        for server in server_list:
+#            print frame.rstrip() + ' -> ' + server.rstrip()
+#            #quick ping test
+#            response = ping_server.ping(server)
+#            #typically 0 = False, but not for ping apparently
+#            if response == 0:
+#                #server is active, let's ssh to it
+#                client = SSHClient()
+#                client.load_system_host_keys()
+#                server_is_active=1
+#                try:
+#                    client.connect(str(server).rstrip(), username="wrehfiel")
+#
+#                except:
+#                    #can't log in, set it as an exception
+#                    #b = AIXServer(name=server.rstrip(), frame=frame.rstrip(), os='AIX', exception=True)[0]
+#                    try:
+#                        AIXServer.objects.filter(name=server.rstrip()).update(frame=frame.rstrip(), os='AIX', exception=True, modified=timezone.now())
+#                        change_message = "Could not SSH to server. Set exception to True"
+#                        #LogEntry.objects.create(action_time=timezone.now(), user_id=11, content_type_id=9, object_id=264, object_repr=server, action_flag=2, change_message=change_message)
+#                    except:
+##                        AIXServer.objects.get_or_create(name=server.rstrip(), frame=frame.rstrip(), os='AIX', exception=True)
+#                    server_is_active=0
+#                client.close()
+#                if server_is_active:
+#                    #server is good, let's add it to the database.
+#                    #add_server(name=server.rstrip(), frame=frame.rstrip()( os="AIX")
+#                    try:
+#                        AIXServer.objects.filter(name=server.rstrip()).update(frame = frame.rstrip(), os='AIX', exception=False, modified=timezone.now())
+#                    except:
+#                        AIXServer.objects.get_or_create(name=server.rstrip(), frame=frame.rstrip(), os='AIX', exception=False)
+#
+#
+#            else:
+#                #server is inactive, let's flag it
+#                try:
+#                    AIXServer.objects.filter(name=server.rstrip()).update(frame=frame.rstrip(), os='AIX', active=False, modified=timezone.now())
+#                    change_message = "Server is now inactive. Set active to False"
+#                    #LogEntry.objects.create(action_time=timezone.now(), user_id=11, content_type_id=9, object_id=264, object_repr=server, action_flag=2, change_message=change_message)
+#                except:
+#                    AIXServer.objects.get_or_create(name=server.rstrip(), frame=frame.rstrip(), os='AIX', active=False)
+
+
+
+    #for server in exclusion_list:
+    #    Server.objects.filter(name=server).update(exception=True)
+#def add_server(name, frame, os):
+#    s = Server.objects.get_or_create(name=name, fos='AIX')[0]
+#    return s
+
+
+
+#start execution
+if __name__ == '__main__':
+    print "Starting populations..."
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
+    populate()
