5616b39c242fc9c270c7dc3cb2278ea6db6fee7d
Boomer Rehfield <angrek@gmail.com>
2014-09-23 15:33:05 -0400

    added power7 fields and working script for it

--- aix_power7_inventory.py
+++ aix_power7_inventory.py
@@ -4,6 +4,12 @@
 # Django, AIX Power7 inventory scripts. This is for gathering the mem
 #   and cpu stats right now from the HMC and drop it into Django.
 #
+# Ok, for now this may be called from main script (future plans)
+# but the idea is that the server and frame checks will have already
+# been done so if it's pulling it from the frame, then it should already
+# be in the database. I 'think' get_or_create should work here fine..
+# Testing for now....
+#
 # Boomer Rehfield - 9/22/2014
 #
 #########################################################################
@@ -16,8 +22,7 @@ from django.contrib.admin.models import LogEntry
 #these are need in django 1.7 and needed vs the django settings command
 import django
 from dashboard import settings
-from server.models import AIXServer
-import ping_server
+from server.models import AIXServer, Power7Inventory
 import logging
 django.setup()
 
@@ -58,7 +63,7 @@ def populate():
 
 
         #for each frame, let's get all of the HMC memory data
-        command = 'lshwres -r mem -m ' + frame.rstrip() + ' --level lpar'
+        command = 'lshwres -r proc -m ' + frame.rstrip() + ' --level lpar'
         print command
 
         stdin, stdout, stderr = client.exec_command(command)
@@ -74,34 +79,48 @@ def populate():
                     a,b = entry.split('=')
                     lpar_array[a] = b
 
-        for key, value in lpar_array.iteritems():
-            print key, "=>",value
+            #ok, first we want to get the lpar name and then remove it from the dict
+            #NOTE: in the database it is FK to 'name'. When I created the server db
+            #I just called it name so that's why there is a difference. I can't go back
+            #and change it because it is populated with WPARs also.
+            server_name = lpar_array['lpar_name']
+            print server_name
+            #deleting so we can iterate over all of the values
+            del lpar_array['lpar_name']
+           
+            #in case the server is new since the last time it ran, we'll just create a blank server record and then update it.
+            #we don't really need error checking here because it's whatever the HMC gave us and the previous scripts will have added them to the AIXServer database.
+            name = AIXServer.objects.get(name=server_name)
+            print name
+            Power7Inventory.objects.get_or_create(name=name)
+            for key, value in lpar_array.iteritems():
+                print key, "=>",value
+                print "attempting to update value...."
+                Power7Inventory.objects.filter(name=name).update(**{key: value})
 
-
-
-
+###########temp comment block
         #for each frame, let's get all of the HMC CPU data for it
-        command = 'lshwres -r proc -m ' + frame.rstrip() + ' --level lpar'
-        print command
-
-        sdtin, stdout, stderr = client.exec_command(command)
-        lpar_list = stdout.readlines()
-        client.close()
-
-        lpar_array = {}
-        for lpar in lpar_list:
-            print ">>>>>>>> " + lpar
-            print " "
-            lpar_dict = lpar.split(",")
-            for entry in lpar_dict:
-                #test for an empty value
-                if entry:
-                    a,b = entry.split('=')
-                    lpar_array[a] = b
-        
-        for name, possible_items in lpar_array.iteritems():
-            print name, "=>",possible_items 
-
+#        command = 'lshwres -r mem -m ' + frame.rstrip() + ' --level lpar'
+#        print command
+#
+#        sdtin, stdout, stderr = client.exec_command(command)
+#        lpar_list = stdout.readlines()
+#        client.close()
+#
+#        lpar_array = {}
+#        for lpar in lpar_list:
+#            print ">>>>>>>> " + lpar
+#            print " "
+#            lpar_dict = lpar.split(",")
+#            for entry in lpar_dict:
+#                #test for an empty value
+#                if entry:
+#                    a,b = entry.split('=')
+#                    lpar_array[a] = b
+#        
+#        for name, possible_items in lpar_array.iteritems():
+#            print name, "=>",possible_items 
+##############
 
         #Ok, here's the break in logic from the others. We don't need to hit the servers.
         #For each frame, run the lshwres command and that will spit out the data for
