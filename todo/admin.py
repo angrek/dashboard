ec98d8a0af588ab406380457def1b63f1459b31e
Boomer Rehfield <angrek@gmail.com>
2014-10-06 15:59:45 -0400

    changed created_by to read only

--- todo/admin.py
+++ todo/admin.py
@@ -3,9 +3,10 @@ from todo.models import Item, User, List, Comment
 
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('title', 'list', 'priority', 'assigned_to', 'completed', 'due_date')
+    list_display = ('title', 'list', 'priority', 'created_by', 'assigned_to', 'completed', 'due_date')
     list_editable = ('list', 'priority', 'completed', 'assigned_to', 'due_date')
     list_filter = ('list', 'completed', 'priority', 'assigned_to', 'due_date')
+    readonly_fields = ('created_by',)
     ordering = ('completed', 'priority',)
     search_fields = ('name',)
     save_on_top = True
