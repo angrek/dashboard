57c4891088dbf98eff463ebb2b65726951fbb942
Boomer Rehfield <angrek@gmail.com>
2014-10-07 10:59:45 -0400

    in a loop here...wth

---
+++ todo/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from todo.models import Item, User, List, Comment, PersonalTodo
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'list', 'priority', 'created_by', 'assigned_to', 'completed', 'due_date')
+    list_editable = ('list', 'priority', 'completed', 'assigned_to', 'due_date')
+    list_filter = ('list', 'completed', 'priority', 'assigned_to', 'due_date')
+    readonly_fields = ('created_by',)
+    ordering = ('completed', 'priority',)
+    search_fields = ('name',)
+    save_on_top = True
+<<<<<<< HEAD
+
+class PersonalTodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'list', 'priority', 'created_by', 'assigned_to', 'completed', 'due_date')
+    list_editable = ('list', 'priority', 'completed', 'assigned_to', 'due_date')
+    list_filter = ('list', 'completed', 'priority', 'assigned_to', 'due_date')
+    readonly_fields = ('created_by',)
+    ordering = ('completed', 'priority',)
+    search_fields = ('name',)
+    save_on_top = True
+
+    def queryset(self, request):
+        qs = super(PersonalTodoAdmin, self).queryset(request)
+        #if request.user.is_superuser:
+        #    return qs
+        return qs.filter(assigned_to=request.user)
+=======
+>>>>>>> ec98d8a0af588ab406380457def1b63f1459b31e
+
+admin.site.register(List)
+admin.site.register(Comment)
+admin.site.register(Item, ItemAdmin)
+<<<<<<< HEAD
+admin.site.register(PersonalTodo, PersonalTodoAdmin)
+=======
+>>>>>>> ec98d8a0af588ab406380457def1b63f1459b31e
