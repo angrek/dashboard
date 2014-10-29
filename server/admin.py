ad794790852188e1ceb918a8fb0ed1a0bc1e894c
Boomer Rehfield <angrek@gmail.com>
2014-10-29 09:36:42 -0400

    removed zone because of an icontains error that was because of the foreign key...probably was not needed anyway

--- server/admin.py
+++ server/admin.py
@@ -11,7 +11,7 @@ class AIXServerAdmin(admin.ModelAdmin):
     save_on_top = True
     list_display = ['name', 'frame', 'ip_address', 'zone', 'active','exception', 'modified', 'os', 'os_level', 'centrify', 'xcelys', 'bash', 'ssl']
     list_filter = ['frame', 'os', 'os_level', 'zone', 'active', 'exception', 'centrify', 'xcelys', 'bash', 'ssl']
-    search_fields = ['name', 'frame', 'ip_address', 'os', 'os_level', 'zone', 'centrify', 'xcelys', 'bash', 'ssl']
+    search_fields = ['name', 'frame', 'ip_address', 'os', 'os_level', 'centrify', 'xcelys', 'bash', 'ssl']
     readonly_fields = ['created', 'modified']
     fields = ['name', 'frame', 'active', 'exception', 'created', 'modified', 'zone', 'ip_address', 'os', 'os_level', 'centrify', 'xcelys', 'bash', 'ssl', 'java', 'log']
 
