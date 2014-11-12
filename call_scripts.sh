92010ef5d816cf7d6a051e9e32f5a52c5edbe3a7
Boomer Rehfield <angrek@gmail.com>
2014-11-12 09:43:34 -0500

    adding script again

---
+++ call_scripts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#script to activate virtualenv and run all of the other scripts in order
+
+cd /home/wrehfiel/ENV/
+#source bin/activate
+
+export DJANGO_SETTINGS_MODULE=dashboard.settings
+/home/wrehfiel/ENV/bin/python dashboard/aix_bash.py
+mail william.rehfield@wellcare.com < 1
+/home/wrehfiel/ENV/bin/python dashboard/aix_centrify.py
+mail william.rehfield@wellcare.com < 2
+python dashboard/aix_errpt.py
+mail william.rehfield@wellcare.com < 3
+python dashboard/aix_imperva.py
+mail william.rehfield@wellcare.com < 4
+python dashboard/aix_oslevel.py
+mail william.rehfield@wellcare.com < 5
+python dashboard/aix_ssl.py
+mail william.rehfield@wellcare.com < 6
+python dashboard/aix_storage.py
+mail william.rehfield@wellcare.com < 7
+python dashboard/aix_xcelys.py
+mail william.rehfield@wellcare.com < 8
+
+
+#put something in here to shoot me an email
