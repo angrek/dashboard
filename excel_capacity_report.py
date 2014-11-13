ad92eeab0f8a1744f47471c580d318cd640a43ae
Boomer Rehfield <angrek@gmail.com>
2014-11-13 16:02:13 -0500

    fixing the report and changing the name to add the date

--- excel_capacity_report.py
+++ excel_capacity_report.py
@@ -11,12 +11,11 @@
 import os
 import re
 from ssh import SSHClient
-import paramiko
 from openpyxl import Workbook
 from openpyxl.styles import Style, PatternFill, Border, Side, Alignment, Protection, Font
 
 from django.utils import timezone
-from server.models import AIXServer, Power7Inventory, Storage
+from server.models import AIXServer, LinuxServer, Power7Inventory, Storage
 
 #these are need in django 1.7 and needed vs the django settings command
 import django
@@ -36,7 +35,7 @@ ws2 = wb.create_sheet()
 ws2.title = 'Linux'
 
 
-#create the header
+#create le pretty headers
 ws1['A1'] = 'Host Name'
 ws1['B1'] = 'OS'
 ws1['C1'] = 'Physical/VM'
@@ -61,15 +60,24 @@ ws1.column_dimensions["C"].width = 20
 ws1.column_dimensions["F"].width = 20
 ws1.column_dimensions["H"].width = 12
 
+ws2.column_dimensions["A"].width = 20
+ws2.column_dimensions["B"].width = 10
+ws2.column_dimensions["C"].width = 20
+ws2.column_dimensions["F"].width = 20
+ws2.column_dimensions["H"].width = 12
+
 list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
 #to hell with writing all this out because the row dimensions are working...
 for letter in list:
     cell = letter + '1'
     c = ws1[cell]
+    f = ws2[cell]
     c.style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    f.style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
 
 d = ws1.row_dimensions[1].height = 20
+g = ws2.row_dimensions[1].height = 20
 
 def get_server_data():
 #starting line = 3
@@ -119,7 +127,7 @@ def get_server_data():
         ws1[cell] = ''
 
         cell = 'G' + str(line)
-        ws1[cell] = p.size
+        ws1[cell] = str(p.size)
 
         cell = 'H' + str(line)
         ws1[cell] = r.curr_procs
@@ -129,30 +137,104 @@ def get_server_data():
         #AIXServer.objects.filter(name=server).update(active=False, modified=timezone.now())
         line += 1
 
+
+    #################STARTING LINUX SECTION############################
+    line = 3
+    counter = 0
+    linux_server_list = LinuxServer.objects.all()
+    #shortening this just to speed up testing
+    #server_list = AIXServer.objects.filter(name__contains='vio')
+    for server in linux_server_list:
+
+        #FIXME just remove this, this was just so I knew how much longer it was running
+        counter = counter + 1
+
+        t = LinuxServer.objects.get(name=server)
+        if t.active == False:
+            #we don't care about inactive servers for capacity planning
+            continue
+
+        #try:
+        #    r = Power7Inventory.objects.get(name=server)
+        #except:
+        #    pass
+        #
+        #try:
+        #    p = Storage.objects.get(name=server)
+        #except:
+        #    pass
+
+        print str(counter) + ',' + str(server) + ',Linux,VM,' + t.ip_address.rstrip() + ',' + str(t.memory) + ',,' + ' n/a' + ',' + str(t.cpu)
+
+        cell = 'A' + str(line)
+        ws2[cell] = str(server)
+
+        cell = 'B' + str(line)
+        ws2[cell] = 'Linux'
+
+        cell = 'C' + str(line)
+        ws2[cell] = 'VM'
+
+        cell = 'D' + str(line)
+        ws2[cell] = t.ip_address.rstrip()
+
+        cell = 'E' + str(line)
+        ws2[cell] = t.memory
+
+        cell = 'F' + str(line)
+        ws2[cell] = ''
+
+        cell = 'G' + str(line)
+        ws2[cell] = ''
+
+        cell = 'H' + str(line)
+        ws2[cell] = t.cpu
+
+
+
+        #AIXServer.objects.filter(name=server).update(active=False, modified=timezone.now())
+        line += 1
+
+
+
     #total
     cell = 'A' + str(line + 1)
     ws1[cell] = 'Total'
+    ws2[cell] = 'Total'
     ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    ws2[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
 
     cell = 'E' + str(line + 1)
     sum = "=SUM(E3:E" + str((line - 1)) + ")"
     ws1[cell] = sum
+    ws2[cell] = sum
     ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    ws2[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
 
     cell = 'G' + str(line + 1)
     sum = "=SUM(G3:G" + str((line - 1)) + ")"
     ws1[cell] = sum
+    ws2[cell] = sum
     ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    ws2[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
 
     cell = 'H' + str(line + 1)
     sum = "=SUM(H3:H" + str((line - 1)) + ")"
     ws1[cell] = sum
+    ws2[cell] = sum
     ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    ws2[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
 
-    wb.save('Unix_Capacity_Planning_Report.xlsx')
+    #need to put the timestamp in the filename
+    now = timezone.now()
+    timestamp = now.strftime('%m-%d-%Y')
+    filename = 'Unix_Configuration_Report' + timestamp + '.xlsx'
+    wb.save(filename)
 
 
+    #c and f are just arbitrary vars right now for page one and two :\
     c.style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    f.style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
 
 
 
@@ -164,8 +246,14 @@ def get_server_data():
 #start execution
 if __name__ == '__main__':
     print "Getting server information..."
-    start_time = timezone.now()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
     get_server_data()
-    elapsed_time = timezone.now() - start_time
-    print "Elapsed time: " + str(elapsed_time)
+
+    #print "Elapsed time: " + str(elapsed_time)
+
+
+
+
+
+
+
