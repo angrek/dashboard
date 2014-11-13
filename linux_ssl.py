df1e5b345baec653b7e8071c8869e36fd5e38b65
Boomer Rehfield <angrek@gmail.com>
2014-11-13 14:54:38 -0500

    Linux ssl needed a different command and cleaned it up a little

--- linux_ssl.py
+++ linux_ssl.py
@@ -22,14 +22,11 @@ django.setup()
 
 def update_server():
     counter = 0
-    server_list = AIXServer.objects.all()
+    server_list = LinuxServer.objects.all()
     for server in server_list:
         server_is_active = 1
 
-        #FIXME just remove this, this was just so I knew how much longer it was running
-        counter = counter + 1
-        #print str(counter) + " - " + str(server)
-        if AIXServer.objects.filter(name=server, active=True, exception=False):
+        if LinuxServer.objects.filter(name=server, active=True, exception=False):
             
             response = ping_server.ping(server)
             #ping test
@@ -43,22 +40,20 @@ def update_server():
                     #FIXME - need to check and see if it was an exception before and make it one if it wasn't
                     continue
 
-                stdin, stdout, stderr = client.exec_command('lslpp -l | grep -i openssl.base')
+                stdin, stdout, stderr = client.exec_command('rpm -qa | grep openssl | grep -v devel | uniq')
                 #this is going to pull 4 different parts of ssl, we just need the base
                 rows = stdout.readlines()
-                if rows:
-                    row = rows[0]
-                    #split the lines and grab the first one
-                    temp = row.split("\r")[0]
-                    p = re.compile(r' +')
-                    temp2 = p.split(temp)
-                    ssl = temp2[2]
-                    
-                    #if existing value is the same, don't update
-                    if str(ssl) != str(server.ssl):
-                        AIXServer.objects.filter(name=server, exception=False, active=True).update(ssl=ssl, modified=timezone.now())
+                ssl = str(rows[0])
+
+                #cutton off the beginning and end, not really needed.
+                ssl = re.sub('openssl-', '', ssl)
+                ssl = re.sub('.x86_64', '', ssl)
+
+                #if existing value is the same, don't update
+                if str(ssl) != str(server.ssl):
+                    LinuxServer.objects.filter(name=server, exception=False, active=True).update(ssl=ssl, modified=timezone.now())
             else:
-                AIXServer.objects.filter(name=server).update(active=False, modified=timezone.now())
+                LinuxServer.objects.filter(name=server).update(active=False, modified=timezone.now())
                 #print str(server) + ' not responding to ping, setting to inactive.'
                 LogEntry.objects.create(action_time=timezone.now(), user_id=11, content_type_id=9, object_id =264, object_repr=server, action_flag=2, change_message='Ping failed, changed to inactive.')
 
@@ -70,7 +65,7 @@ if __name__ == '__main__':
     print "Checking SSL versions..."
     start_time = timezone.now()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
-    from server.models import AIXServer
+    from server.models import LinuxServer
     update_server()
     elapsed_time = timezone.now() - start_time
     print "Elapsed time: " + str(elapsed_time)
