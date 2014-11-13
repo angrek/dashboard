34a83241a2dacd37eed8cac8c0aaa9f0dbf037af
Boomer Rehfield <angrek@gmail.com>
2014-11-13 10:54:49 -0500

    uncommented other jobs and added email subjects

--- call_scripts.sh
+++ call_scripts.sh
@@ -6,22 +6,22 @@ cd /home/wrehfiel/ENV/
 #source bin/activate
 
 export DJANGO_SETTINGS_MODULE=dashboard.settings
-/home/wrehfiel/ENV/bin/python dashboard/aix_bash.py
-mail william.rehfield@wellcare.com < 1.txt
-/home/wrehfiel/ENV/bin/python dashboard/aix_centrify.py
-mail william.rehfield@wellcare.com < 2.txt
-python dashboard/aix_errpt.py
-mail william.rehfield@wellcare.com < 3.txt
-python dashboard/aix_imperva.py
-mail william.rehfield@wellcare.com < 4.txt
-python dashboard/aix_oslevel.py
-mail william.rehfield@wellcare.com < 5.txt
-python dashboard/aix_ssl.py
-mail william.rehfield@wellcare.com < 6.txt
-python dashboard/aix_storage.py
-mail william.rehfield@wellcare.com < 7.txt
-python dashboard/aix_xcelys.py
-mail william.rehfield@wellcare.com < 8.txt
+/home/wrehfiel/ENV/dashboard/aix_bash.py
+mail william.rehfield@wellcare.com -s aix_bash < 1.txt
+/home/wrehfiel/ENV/dashboard/aix_centrify.py
+mail william.rehfield@wellcare.com -s aix_centrify < 2.txt
+/home/wrehfiel/ENV/dashboard/aix_errpt.py
+mail william.rehfield@wellcare.com -s aix_errpt < 3.txt
+/home/wrehfiel/ENV/dashboard/aix_imperva.py
+mail william.rehfield@wellcare.com -s aix_imperva < 4.txt
+/home/wrehfiel/ENV/dashboard/aix_oslevel.py
+mail william.rehfield@wellcare.com -s aix_oslevel < 5.txt
+/home/wrehfiel/ENV/dashboard/aix_ssl.py
+mail william.rehfield@wellcare.com -s aix_ssl < 6.txt
+/home/wrehfiel/ENV/dashboard/aix_storage.py
+mail william.rehfield@wellcare.com -s aix_storage < 7.txt
+/home/wrehfiel/ENV/dashboard/aix_xcelys.py
+mail william.rehfield@wellcare.com -s aix_xcelys < 8.txt
 
 
 #put something in here to shoot me an email
