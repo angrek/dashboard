01c18dbb2080fee8cb76d0aa09a70dcaa1640584
Boomer Rehfield <angrek@gmail.com>
2014-11-13 14:34:11 -0500

    changed the imperva script work with linux

--- linux_imperva.py
+++ linux_imperva.py
@@ -14,25 +14,24 @@ from django.contrib.admin.models import LogEntry
 #these are need in django 1.7 and needed vs the django settings command
 import django
 from dashboard import settings
-from server.models import AIXServer
+from server.models import LinuxServer
 import ping_server
 django.setup()
 import re
 
 
 def update_server():
-    server_list = AIXServer.objects.all()
+    server_list = LinuxServer.objects.all()
     #FIXME quick way of testing a few servers
-    #server_list = AIXServer.objects.filter(name='d1bwadb')
-    #server_list = ['d1vio01', 'd1vio01']
-    #server_list = AIXServer.objects.filter(name__contains='db')
+    #server_list = LinuxServer.objects.filter(name='d1bwadb')
+    #server_list = LinuxServer.objects.filter(name__contains='db')
     counter = 0
     for server in server_list:
         counter += 1
         print str(counter) + ' - ' + str(server)
         server_is_active=1
 
-        if AIXServer.objects.filter(name=server, active=True, exception=False):
+        if LinuxServer.objects.filter(name=server, active=True, exception=False):
             response = ping_server.ping(server)
             if response == 0:
                 #SSHClient.util.log_to_file('test.log')
@@ -43,7 +42,7 @@ def update_server():
                     client.connect(str(server), username="wrehfiel")
                 except:
                     print 'SSH to ' + str(server) + ' failed, changing exception'
-                    #AIXServer.objects.filter(name=server).update(exception=True, modified=timezone.now())
+                    #LinuxServer.objects.filter(name=server).update(exception=True, modified=timezone.now())
                     
                     #LogEntry.objects.create(action_time='2014-08-25 20:00:00', user_id=11, content_type_id=9, object_id =264, object_repr=server, action
                     #LogEntry.objects.create(action_time=timezone.now(), user_id=11, content_type_id=9, object_id =264, object_repr=server, action_flag=2, change_message='SSH failed, changed exception.')
@@ -69,17 +68,17 @@ def update_server():
                         print imperva_version
                         #check existing value, if it exists, don't update
                         if str(imperva_version) != str(server.imperva):
-                            AIXServer.objects.filter(name=server).update(imperva=imperva_version, modified=timezone.now())
+                            LinuxServer.objects.filter(name=server).update(imperva=imperva_version, modified=timezone.now())
                             #pretty user the timestamp is auto created even though the table doesn't reflect it... maybe it's in the model
                             change_message = 'Changed imperva version to ' + str(imperva_version)
                             LogEntry.objects.create(action_time='2014-08-25 20:00:00', user_id=11, content_type_id=9, object_id=264, object_repr=server, action_flag=2, change_message=change_message)
                             #FIXME - ok, we're going to create the manual log here, haven't worked it all out yet how I want to do it though
                             #We can do that or we can FK to the admin log...should we try to add our own columns?
-                            #log = AIXServer.objects.log(name=server 
+                            #log = LinuxServer.objects.log(name=server 
                     except:
                         imperva_version = 'Not installed'
                         if str(imperva_version) != str(server.imperva):
-                            AIXServer.objects.filter(name=server).update(imperva=imperva_version, modified=timezone.now())
+                            LinuxServer.objects.filter(name=server).update(imperva=imperva_version, modified=timezone.now())
                             #pretty user the timestamp is auto created even though the table doesn't reflect it... maybe it's in the model
                             change_message = 'Changed imperva version to ' + str(imperva_version)
                             LogEntry.objects.create(action_time='2014-08-25 20:00:00', user_id=11, content_type_id=9, object_id=264, object_repr=server, action_flag=2, change_message=change_message)
@@ -87,7 +86,7 @@ def update_server():
 
             else:
                 print str(server) + ' not responding to ping, setting to inactive.'
-                AIXServer.objects.filter(name=server, exception=False, active=True).update(active=False, modified=timezone.now())
+                LinuxServer.objects.filter(name=server, exception=False, active=True).update(active=False, modified=timezone.now())
                 #FIXME I need a check here otherwise it isn't really a change, it's updating the same value
                 LogEntry.objects.create(action_time=timezone.now(), user_id=11 ,content_type_id=9, object_id =264, object_repr=server, action_flag=2, change_message='Ping failed, changed to inactive.')
 
