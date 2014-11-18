dffa6c5e6069f4e6a63100868ca7849365b9d07a
Boomer Rehfield <angrek@gmail.com>
2014-11-18 10:15:03 -0500

    trying to get storage to work, still broken

--- linux_storage.py
+++ linux_storage.py
@@ -1,99 +1,95 @@
 #!/home/wrehfiel/ENV/bin/python2.7
 #########################################################################
 #
-# Script to storage size of each of the Linux servers
+# Script to retrieve bash versions and drop them into Django dashboard
 #
 # Boomer Rehfield - 11/13/2014
 #
 #########################################################################
 
-import os
+import os, re
 from ssh import SSHClient
 from django.utils import timezone
 from django.contrib.admin.models import LogEntry
 #these are need in django 1.7 and needed vs the django settings command
 import django
 from dashboard import settings
-from server.models import AIXServer, Storage
+from server.models import LinuxServer
 import ping_server
 django.setup()
 
 
-
 def update_server():
-    #right now we are just getting these for the VIO servers
-    #server_list = AIXServer.objects.filter(name__contains='auto').exclude(name__contains='vio')
-    server_list = AIXServer.objects.all().exclude(name__contains='vio')
-    #FIXME need to exclude vio servers
-    #server_list = AIXServer.objects.filter(name__contains='p1vio01')
-    counter  = 1
+    #server_list = LinuxServer.objects.all()
+    #FIXME quick way of testing a few servers
+    server_list = LinuxServer.objects.filter(name='ustsesbapp')
+    #server_list = ['d1vio01', 'd1vio01']
+    #server_list = LinuxServer.objects.filter(name__contains='vio')
+    counter = 0
     for server in server_list:
+        counter += 1
+        print str(counter) + ' - ' + str(server)
         server_is_active=1
 
-        #Make sure the server is set to active and not an exception
-        if AIXServer.objects.filter(name=server, active=True, exception=False):
+        if LinuxServer.objects.filter(name=server, active=True):
             response = ping_server.ping(server)
-            
-            #typically = is false, but that's what ping gives back for a positive
             if response == 0:
+                #SSHClient.util.log_to_file('test.log')
                 client = SSHClient()
                 client.load_system_host_keys()
 
-                #without try, it will break the script if it can't SSH
+                #redundant testing...ssh won't connect for some reason
+                #server = str(server).rstrip()
+                #print server
+                #print len(server)
                 try:
                     client.connect(str(server), username="wrehfiel")
                 except:
                     print 'SSH to ' + str(server) + ' failed, changing exception'
-                    AIXServer.objects.filter(name=server).update(exception=True, modified=timezone.now())
-
-                    #LogEntry.objects.create(action_time='2014-08-25 20:00:00', user_id=11, content_type_id=9, object_id =264, object_repr=server, action
+                    LinuxServer.objects.filter(name=server).update(exception=True, modified=timezone.now())
+                    
                     LogEntry.objects.create(action_time=timezone.now(), user_id=11, content_type_id=9, object_id =264, object_repr=server, action_flag=2, change_message='SSH failed, changed exception.')
-                    continue 
+                    server_is_active=0 
+
+                if server_is_active:
+                    command = 'sudo /sbin/fdisk -l | grep Disk'
+                    stdin, stdout, stderr = client.exec_command(command)
+                    print stdout
+                    size = stdout.readlines()
+                    for line in (size):
+                        print line
+                    print size
+                    print 'Size' + str(size[1])
+                    exit
+                                     
+                    bash_version = re.sub(r'x86_64', '', bash_version)
+
+                    #check existing value, if it exists, don't update
+                    if str(bash_version) != str(server.bash):
+                        LinuxServer.objects.filter(name=server, exception=False, active=True).update(bash=bash_version, modified=timezone.now())
+                        #pretty user the timestamp is auto created even though the table doesn't reflect it... maybe it's in the model
+                        change_message = 'Changed bash version to ' + str(bash_version)
+                        LogEntry.objects.create(action_time='2014-08-25 20:00:00', user_id=11, content_type_id=9, object_id=264, object_repr=server, action_flag=2, change_message=change_message)
+                        #FIXME - ok, we're going to create the manual log here, haven't worked it all out yet how I want to do it though
+                        #We can do that or we can FK to the admin log...should we try to add our own columns?
+                        #log = LinuxServer.objects.log(name=server 
 
 
-                stdin, stdout, stderr = client.exec_command('sudo /scripts/dashboard_disksize.sh')
-                #stdin, stdout, stderr = client.exec_command('ls /')
-
-                #we have to do errpt differently due to the way it is handled by stdout
-                print '======================'
-                print str(counter) + ' - '  + str(server)
-                for line in stdout:
-                    print line.rstrip()
-                
-                counter += 1
-                try:
-                    Storage.objects.get(name=server)
-                    Storage.objects.filter(name=server).update(size=line.rstrip())
-                except:
-                    Storage.objects.get_or_create(name=server, size=line.rstrip())
-
-                #    print '1'
-                #except:
-                #    print '2'
-                #if report == '':
-                #    report = "The errpt was empty."
-              
-                #let's get the PK for the server
-                #server_name = AIXServer.objects.get(name=server)
-                #we don't care about the old record and we'll just overwrite it
-                #Errpt.objects.get_or_create(name=server, report=report, modified=timezone.now())
-                #change_message = 'Updated errpt.'
-                #LogEntry.objects.create(action_time=timezone.now(), user_id=11, content_type_id=9, object_id=264, object_repr=server, action_flag=2, change_message=change_message)
             else:
-                AIXServer.objects.filter(name=server).update(active=False)
                 print str(server) + ' not responding to ping, setting to inactive.'
-                AIXServer.objects.filter(name=server, exception=False, active=True).update(modified=timezone.now())
-                LogEntry.objects.create(action_time=timezone.now(), user_id=11, content_type_id=9, object_id =264, object_repr=server, action_flag=2, change_message='Ping failed, changed to inactive.')
+                LinuxServer.objects.filter(name=server, exception=False, active=True).update(active=False, modified=timezone.now())
+                #FIXME I need a check here otherwise it isn't really a change, it's updating the same value
+                LogEntry.objects.create(action_time=timezone.now(), user_id=11 ,content_type_id=9, object_id =264, object_repr=server, action_flag=2, change_message='Ping failed, changed to inactive.')
 
 
 
 
 #start execution
 if __name__ == '__main__':
-    print "Storage size for each server..."
-    start_time = timezone.now()
+    print "Checking Bash versions..."
+    starting_time = timezone.now()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
-    from server.models import AIXServer
     update_server()
-    elapsed_time = timezone.now() - start_time
+    elapsed_time = timezone.now() - starting_time 
     print "Elapsed time: " + str(elapsed_time)
+
