afa36f841c0869d4c334c02ad571d4f7a1104f24
Boomer Rehfield <angrek@gmail.com>
2014-11-19 08:54:12 -0500

    Got the linux oslevel script working

--- linux_oslevel.py
+++ linux_oslevel.py
@@ -8,28 +8,29 @@
 #########################################################################
 
 import os
+import re
 from ssh import SSHClient
 from django.utils import timezone
 from django.contrib.admin.models import LogEntry
 #these are need in django 1.7 and needed vs the django settings command
 import django
 from dashboard import settings
-from server.models import AIXServer
+from server.models import LinuxServer
 import ping_server
 django.setup()
 
 
 def update_server():
-    server_list = AIXServer.objects.all()
+    server_list = LinuxServer.objects.all()
     #FIXME quick way of testing a few servers
-    #server_list = AIXServer.objects.filter(name='d2vio01')
+    #server_list = LinuxServer.objects.filter(name='d0mwcdb')
     #server_list = ['d1vio01', 'd1vio01']
-    #server_list = AIXServer.objects.filter(name__contains='vio')
+    #server_list = LinuxServer.objects.filter(name__contains='vio')
     for server in server_list:
         #print server
         server_is_active=1
 
-        if AIXServer.objects.filter(name=server, active=True, exception=False):
+        if LinuxServer.objects.filter(name=server, active=True, exception=False):
             response = ping_server.ping(server)
             if response == 0:
                 client = SSHClient()
@@ -39,38 +40,49 @@ def update_server():
                     client.connect(str(server), username="wrehfiel")
                 except:
                     #print 'SSH to ' + str(server) + ' failed, changing exception'
-                    AIXServer.objects.filter(name=server).update(exception=True, modified=timezone.now())
+                    LinuxServer.objects.filter(name=server).update(exception=True, modified=timezone.now())
                     
                     #LogEntry.objects.create(action_time='2014-08-25 20:00:00', user_id=11, content_type_id=9, object_id =264, object_repr=server, action
                     LogEntry.objects.create(action_time=timezone.now(), user_id=11, content_type_id=9, object_id =264, object_repr=server, action_flag=2, change_message='SSH failed, changed exception.')
                     server_is_active=0 
 
                 if server_is_active:
-                    #with the vio servers we want the ios.level rather than the os_level
-                    vio_servers = AIXServer.objects.filter(name__contains='vio')
-                    if server in vio_servers:
-                        command = 'cat /usr/ios/cli/ios.level'
-                    else:
-                        command = 'oslevel -s'
+                    command = 'lsb_release -a | grep Distributor'
                     stdin, stdout, stderr = client.exec_command(command)
 
                     #need rstrip() because there are extra characters at the end
+                    os = stdout.readlines()[0].rstrip()
+                    os = re.sub('Distributor ID:', '', os)
+                    os = re.sub('\s*', '', os)
+
+
+                    if os == 'RedHatEnterpriseServer':
+                        os = 'RHEL'
+                    else:
+                        os = 'Unknown'
+
+
+                    command = 'lsb_release -a | grep Release'
+                    stdin, stdout, stderr = client.exec_command(command)
+
                     oslevel = stdout.readlines()[0].rstrip()
+                    oslevel = re.sub('Release:', '', oslevel)
+                    oslevel = re.sub('\s*', '', oslevel)
 
                     #check existing value, if it exists, don't update
-                    if str(oslevel) != str(server.os_level):
-                        AIXServer.objects.filter(name=server, exception=False, active=True).update(os_level=oslevel, modified=timezone.now())
+                    if str(oslevel) != str(server.os_level) or str(os) != str(server.os):
+                        LinuxServer.objects.filter(name=server, exception=False, active=True).update(os=os, os_level=oslevel, modified=timezone.now())
                         #pretty user the timestamp is auto created even though the table doesn't reflect it... maybe it's in the model
-                        change_message = 'Changed os_level to ' + str(oslevel)
+                        change_message = 'Changed os_level to ' + +str(os) + ' ' + str(oslevel)
                         LogEntry.objects.create(action_time='2014-08-25 20:00:00', user_id=11, content_type_id=9, object_id=264, object_repr=server, action_flag=2, change_message=change_message)
                         #FIXME - ok, we're going to create the manual log here, haven't worked it all out yet how I want to do it though
                         #We can do that or we can FK to the admin log...should we try to add our own columns?
-                        #log = AIXServer.objects.log(name=server 
+                        #log = LinuxServer.objects.log(name=server 
 
 
             else:
                 #print str(server) + ' not responding to ping, setting to inactive.'
-                AIXServer.objects.filter(name=server, exception=False, active=True).update(active=False, modified=timezone.now())
+                LinuxServer.objects.filter(name=server, exception=False, active=True).update(active=False, modified=timezone.now())
                 #FIXME I need a check here otherwise it isn't really a change, it's updating the same value
                 LogEntry.objects.create(action_time=timezone.now(), user_id=11 ,content_type_id=9, object_id =264, object_repr=server, action_flag=2, change_message='Ping failed, changed to inactive.')
 
