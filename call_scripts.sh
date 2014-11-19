84122c4a6cbb59b88ee41781cf486c3f7d1f7271
Boomer Rehfield <angrek@gmail.com>
2014-11-19 14:16:04 -0500

    adding linux scripts and fixing email

--- call_scripts.sh
+++ call_scripts.sh
@@ -2,7 +2,7 @@
 
 #script to activate virtualenv and run all of the other scripts in order
 
-cd /home/wrehfiel/ENV/
+cd /home/wrehfiel/ENV/dashboard
 #source bin/activate
 
 export DJANGO_SETTINGS_MODULE=dashboard.settings
@@ -22,6 +22,25 @@ mail william.rehfield@wellcare.com -s aix_ssl < 6.txt
 mail william.rehfield@wellcare.com -s aix_storage < 7.txt
 /home/wrehfiel/ENV/dashboard/aix_xcelys.py
 mail william.rehfield@wellcare.com -s aix_xcelys < 8.txt
+/home/wrehfiel/ENV/dashboard/aix_netbackup.py
+mail william.rehfield@wellcare.com -s aix_netbackup < 9.txt
 
 
+/home/wrehfiel/ENV/dashboard/linux_bash.py
+mail william.rehfield@wellcare.com -s linux_bash < 9.txt
+/home/wrehfiel/ENV/dashboard/linux_centrify.py
+mail william.rehfield@wellcare.com -s linux_centrify < 9.txt
+/home/wrehfiel/ENV/dashboard/linux_imperva.py
+mail william.rehfield@wellcare.com -s linux_imperva < 9.txt
+/home/wrehfiel/ENV/dashboard/linux_netbackup.py
+mail william.rehfield@wellcare.com -s linux_netbackup < 9.txt
+/home/wrehfiel/ENV/dashboard/linux_oslevel.py
+mail william.rehfield@wellcare.com -s linux_oslevel < 9.txt
+/home/wrehfiel/ENV/dashboard/linux_ssl.py
+mail william.rehfield@wellcare.com -s linux_ssl < 9.txt
+#/home/wrehfiel/ENV/dashboard/linux_storage.py
+mail william.rehfield@wellcare.com -s linux_storage < 9.txt
+/home/wrehfiel/ENV/dashboard/linux_xcelys.py
+mail william.rehfield@wellcare.com -s linux_xcelys < 9.txt
 #put something in here to shoot me an email
+mail william.rehfield@wellcare.com -s 'finished all scripts' < 9.txt
