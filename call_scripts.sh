3aecf9a88e67c2484ba1217457b7689fc37178b3
Boomer Rehfield <angrek@gmail.com>
2014-12-03 16:40:39 -0500

    just combining the text files into one

--- call_scripts.sh
+++ call_scripts.sh
@@ -9,42 +9,42 @@ export DJANGO_SETTINGS_MODULE=dashboard.settings
 /home/wrehfiel/ENV/dashboard/aix_bash.py
 mail william.rehfield@wellcare.com -s aix_bash < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_centrify.py
-mail william.rehfield@wellcare.com -s aix_centrify < 2.txt
+mail william.rehfield@wellcare.com -s aix_centrify < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_errpt.py
-mail william.rehfield@wellcare.com -s aix_errpt < 3.txt
+mail william.rehfield@wellcare.com -s aix_errpt < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_imperva.py
-mail william.rehfield@wellcare.com -s aix_imperva < 4.txt
+mail william.rehfield@wellcare.com -s aix_imperva < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_oslevel.py
-mail william.rehfield@wellcare.com -s aix_oslevel < 5.txt
+mail william.rehfield@wellcare.com -s aix_oslevel < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_ssl.py
-mail william.rehfield@wellcare.com -s aix_ssl < 6.txt
+mail william.rehfield@wellcare.com -s aix_ssl < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_storage.py
-mail william.rehfield@wellcare.com -s aix_storage < 7.txt
+mail william.rehfield@wellcare.com -s aix_storage < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_xcelys.py
-mail william.rehfield@wellcare.com -s aix_xcelys < 8.txt
+mail william.rehfield@wellcare.com -s aix_xcelys < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_netbackup.py
-mail william.rehfield@wellcare.com -s aix_netbackup < 9.txt
+mail william.rehfield@wellcare.com -s aix_netbackup < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_ssh.py
-mail william.rehfield@wellcare.com -s aix_ssh < 9.txt
+mail william.rehfield@wellcare.com -s aix_ssh < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_ssh_cent.py
-mail william.rehfield@wellcare.com -s aix_ssh_cent < 9.txt
+mail william.rehfield@wellcare.com -s aix_ssh_cent < 1.txt
 
 
 /home/wrehfiel/ENV/dashboard/linux_bash.py
-mail william.rehfield@wellcare.com -s linux_bash < 9.txt
+mail william.rehfield@wellcare.com -s linux_bash < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_centrify.py
-mail william.rehfield@wellcare.com -s linux_centrify < 9.txt
+mail william.rehfield@wellcare.com -s linux_centrify < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_imperva.py
-mail william.rehfield@wellcare.com -s linux_imperva < 9.txt
+mail william.rehfield@wellcare.com -s linux_imperva < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_netbackup.py
-mail william.rehfield@wellcare.com -s linux_netbackup < 9.txt
+mail william.rehfield@wellcare.com -s linux_netbackup < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_oslevel.py
-mail william.rehfield@wellcare.com -s linux_oslevel < 9.txt
+mail william.rehfield@wellcare.com -s linux_oslevel < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_ssl.py
-mail william.rehfield@wellcare.com -s linux_ssl < 9.txt
+mail william.rehfield@wellcare.com -s linux_ssl < 1.txt
 #/home/wrehfiel/ENV/dashboard/linux_storage.py
-mail william.rehfield@wellcare.com -s linux_storage < 9.txt
+mail william.rehfield@wellcare.com -s linux_storage < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_xcelys.py
-mail william.rehfield@wellcare.com -s linux_xcelys < 9.txt
+mail william.rehfield@wellcare.com -s linux_xcelys < 1.txt
 #put something in here to shoot me an email
-mail william.rehfield@wellcare.com -s 'finished all scripts' < 9.txt
+mail william.rehfield@wellcare.com -s 'finished all scripts' < 1.txt
