aaaeb8069277bc439417be266f6871b801a5f4ad
Boomer Rehfield <angrek@gmail.com>
2014-12-08 11:29:31 -0500

    added EMC version script and put it in the main call script (also cleaned up emails)

---
+++ aix_emc.py
@@ -0,0 +1,86 @@
+#!/home/wrehfiel/ENV/bin/python2.7
+#########################################################################
+#
+# Script to retrieve EMC versions on the servers and drop them into Django dashboard
+#
+# Boomer Rehfield - 8/7/2014
+#
+#########################################################################
+
+import os
+import re
+from ssh import SSHClient
+import paramiko
+from django.utils import timezone
+from django.contrib.admin.models import LogEntry
+#these are need in django 1.7 and needed vs the django settings command
+import django
+from dashboard import settings
+import test_server
+django.setup()
+
+
+def update_server():
+
+    server_list = AIXServer.objects.all()
+    #server_list = AIXServer.objects.filter(name__contains='ufts')
+    for server in server_list:
+
+        if test_server.ping(server):
+
+            client = SSHClient()
+            if test_server.ssh(server, client):
+
+                #get the emc_clariion version
+                stdin, stdout, stderr = client.exec_command('lslpp -l | grep -i EMC.CLARIION.fcp | uniq')
+                rows = stdout.readlines()
+
+                if rows:
+                    row = rows[0]
+                    #split the lines and grab the first one
+                    temp = row.split("\r")[0]
+                    p = re.compile(r' +')
+                    temp2 = p.split(temp)
+                    emc_clar = temp2[2]
+                    print server
+                    print 'emc_clar'
+                    print str(emc_clar)                    
+                    #if existing value is the same, don't update
+                    if str(emc_clar) != str(server.emc_clar):
+                        AIXServer.objects.filter(name=server).update(emc_clar=emc_clar, modified=timezone.now())
+                        change_message = 'Changed EMC_CLARIION version to ' + str(emc_clar)
+                        LogEntry.objects.create(action_time='2014-08-25 20:00:00', user_id=11, content_type_id=9, object_id=264, object_repr=server, action_flag=2, change_message=change_message)
+
+
+                #get the emc_sym disks
+                stdin2, stdout2, stderr2 = client.exec_command('lslpp -l | grep -i EMC.Symmetrix.fcp | uniq')
+                rows2 = stdout2.readlines()
+                if rows2:
+                    row = rows2[1]
+                    temp = row.split("\r")[0]
+                    p = re.compile(r' +')
+                    temp2 = p.split(temp)
+                    emc_sym = temp2[1]
+                    print server
+                    print 'emc_sym'
+                    print str(emc_sym)                    
+                    #if existing value is the same, don't update
+                    if str(emc_sym) != str(server.emc_sym):
+                        AIXServer.objects.filter(name=server).update(emc_sym=emc_sym, modified=timezone.now())
+                        change_message = 'Changed EMC_Symmetrix version to ' + str(emc_sym)
+                        LogEntry.objects.create(action_time='2014-08-25 20:00:00', user_id=11, content_type_id=9, object_id=264, object_repr=server, action_flag=2, change_message=change_message)
+
+
+
+#start execution
+
+
+#start execution
+if __name__ == '__main__':
+    print "Checking EMC versions..."
+    start_time = timezone.now()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
+    from server.models import AIXServer
+    update_server()
+    elapsed_time = timezone.now() - start_time
+    print "Elapsed time: " + str(elapsed_time)
--- call_scripts.sh
+++ call_scripts.sh
@@ -7,44 +7,29 @@ cd /home/wrehfiel/ENV/dashboard
 
 export DJANGO_SETTINGS_MODULE=dashboard.settings
 /home/wrehfiel/ENV/dashboard/aix_bash.py
-mail william.rehfield@wellcare.com -s aix_bash < 1.txt
+mail william.rehfield@wellcare.com -s 'starting aix scripts' < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_centrify.py
-mail william.rehfield@wellcare.com -s aix_centrify < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_errpt.py
-mail william.rehfield@wellcare.com -s aix_errpt < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_imperva.py
-mail william.rehfield@wellcare.com -s aix_imperva < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_oslevel.py
-mail william.rehfield@wellcare.com -s aix_oslevel < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_ssl.py
-mail william.rehfield@wellcare.com -s aix_ssl < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_storage.py
-mail william.rehfield@wellcare.com -s aix_storage < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_xcelys.py
-mail william.rehfield@wellcare.com -s aix_xcelys < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_netbackup.py
-mail william.rehfield@wellcare.com -s aix_netbackup < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_ssh.py
-mail william.rehfield@wellcare.com -s aix_ssh < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_ssh_cent.py
-mail william.rehfield@wellcare.com -s aix_ssh_cent < 1.txt
+/home/wrehfiel/ENV/dashboard/aix_emc.py
+mail william.rehfield@wellcare.com -s 'aix scripts are done' < 1.txt
 
 
+mail william.rehfield@wellcare.com -s 'starting linux scripts' < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_bash.py
-mail william.rehfield@wellcare.com -s linux_bash < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_centrify.py
-mail william.rehfield@wellcare.com -s linux_centrify < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_imperva.py
-mail william.rehfield@wellcare.com -s linux_imperva < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_netbackup.py
-mail william.rehfield@wellcare.com -s linux_netbackup < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_oslevel.py
-mail william.rehfield@wellcare.com -s linux_oslevel < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_ssl.py
-mail william.rehfield@wellcare.com -s linux_ssl < 1.txt
 #/home/wrehfiel/ENV/dashboard/linux_storage.py
-mail william.rehfield@wellcare.com -s linux_storage < 1.txt
 /home/wrehfiel/ENV/dashboard/linux_xcelys.py
-mail william.rehfield@wellcare.com -s linux_xcelys < 1.txt
 #put something in here to shoot me an email
 mail william.rehfield@wellcare.com -s 'finished all scripts' < 1.txt
