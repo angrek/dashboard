ec4df05327b3839295d8cf08955e297e7cab7cc4
Boomer Rehfield <angrek@gmail.com>
2015-01-07 15:31:54 -0500

    Changed blankspace to None

--- linux_xcelys.py
+++ linux_xcelys.py
@@ -13,7 +13,7 @@ from django.utils import timezone
 #these are need in django 1.7 and needed vs the django settings command
 import django
 from dashboard import settings
-from server.models import AIXServer
+from server.models import AIXServer,LinuxServer
 import re
 import test_server
 import dashboard_logging
@@ -37,11 +37,12 @@ def update_server():
 
                 #need to cut the string down
                 xcelys_version = temp_xcelys_version[36:-16]
-                 
+                if xcelys_version is '':
+                    xcelys_version = "None"
                 #check existing value, if it exists, don't update
                 if str(xcelys_version) != str(server.xcelys):
                     dashboard_logging.log_change(str(server), 'Xcelys', str(server.xcelys), str(xcelys_version))
-                    AIXServer.objects.filter(name=server, exception=False, active=True).update(xcelys=xcelys_version, modified=timezone.now())
+                    LinuxServer.objects.filter(name=server, exception=False, active=True).update(xcelys=xcelys_version, modified=timezone.now())
 
 
 
