8ad67149efccad430e7086b2b17b5b23b31df137
Boomer Rehfield <angrek@gmail.com>
2015-03-05 15:22:09 -0500

    Quick mod to single out a few leftover servers

--- aix_rsyslog.py
+++ aix_rsyslog.py
@@ -21,7 +21,7 @@ django.setup()
 def update_server():
 
     #server_list = AIXServer.objects.all()
-    server_list = AIXServer.objects.filter(decommissioned=False, active=True)
+    server_list = AIXServer.objects.filter(decommissioned=False)
     #server_list = AIXServer.objects.filter(name='t8sandbox')
 
     counter = 0
