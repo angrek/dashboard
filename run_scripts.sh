d13e71a1e8f53c5a5cb4ea6f4211eb80191fb276
Boomer Rehfield <angrek@gmail.com>
2015-03-16 10:07:28 -0400

    Added tmef script

--- run_scripts.sh
+++ run_scripts.sh
@@ -13,6 +13,8 @@ mail william.rehfield@wellcare.com -s 'starting aix populate' < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_populate.py
 mail william.rehfield@wellcare.com -s 'starting aix power7 inventory' < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_power7_inventory.py
+mail william.rehfield@wellcare.com -s 'starting aix tmef' < 1.txt
+/home/wrehfiel/ENV/dashboard/aix_tmef.py
 mail william.rehfield@wellcare.com -s 'starting aix powerha' < 1.txt
 /home/wrehfiel/ENV/dashboard/aix_powerha.py
 mail william.rehfield@wellcare.com -s 'starting aix ssh keys' < 1.txt
