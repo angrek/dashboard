28c4dd5bc2569def361a8a862de8ab6410ee265a
Boomer Rehfield <angrek@gmail.com>
2015-03-20 09:21:58 -0400

    More initial timetracker stuff

--- timetracker/urls.py
+++ timetracker/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     url(r'^add_entry$', 'timetracker.views.add_entry', name='add_entry'),
     url(r'^add_report$', 'timetracker.views.add_report', name='add_report'),
     url(r'^show_categories/$', views.show_categories, name='show_categories'),
-    url(r'^show_entries/$', views.show_entries, name='show_entries'),
+    url(r'^show_entries/(?P<date>(.*)+)$', views.show_entries, name='show_entries'),
     url(r'^show_reports/$', views.show_reports, name='show_reports'),
 
 )
