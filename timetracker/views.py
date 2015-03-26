635be52d00343725f7c0249e4c78bfa1df028292
Boomer Rehfield <angrek@gmail.com>
2015-03-26 15:02:55 -0400

    Added total hours per user and total for the day

--- timetracker/views.py
+++ timetracker/views.py
@@ -46,11 +46,12 @@ def add_entry(request):
             hours = request.POST['hours' + str(x)]
             notes = request.POST['notes' + str(x)]
             username = User.objects.get(username=request.user)
-            new_category = Category.objects.get(name=request.POST['category' + str(x)])
+            if description and notes and hours:
+                new_category = Category.objects.get(name=request.POST['category' + str(x)])
 
-            entry = Entry.objects.create(description=description, category=new_category, username=username, hours=hours, notes=notes)
+                entry = Entry.objects.create(description=description, category=new_category, username=username, hours=hours, notes=notes)
         entries = Entry.objects.filter(username=username).order_by('date')
-        return HttpResponseRedirect('show_entries', {'entries': entries})
+        return HttpResponseRedirect('show_entries/0000-00-00', {'entries': entries})
 
     return render(request, 'timetracker/add_entry.html', {
         'form': form,
@@ -190,36 +191,89 @@ def add_category(request):
 def daily_manager_report(request, date):
     temp_list = User.objects.all()
     group_list = []
+
+    #FIXME if multiple groups use this we'll need to grab the manager's group and use that
+    #get all the users in the correct group
     for user in temp_list:
         if user.groups.filter(name='unix').exists():
             group_list.append(user)
-    #group_list = Group.objects.all()
 
     request.GET.get('date')
     if date == '0000-00-00':
         date = datetime.date.today()
     else:
         date = datetime.date(int(date[:4]), int(date[5:7]), int(date[-2:]))
+
+    #get dates for the bread crumbs
     previous = (date - timedelta(days = 1)).strftime('%Y-%m-%d')
     next = (date + timedelta(days = 1)).strftime('%Y-%m-%d')
   
     entries = {}
+    total_hours_by_user = {}
+    total_hours = {}
+    tmp_totals = {}
     users_with_entries = []
     users_with_no_entries = []
+
+    a = Category.objects.get(short_name='A')
+    i = Category.objects.get(short_name='I')
+    o = Category.objects.get(short_name='O')
+    p = Category.objects.get(short_name='P')
+
+    overall_total_a = 0
+    overall_total_i = 0
+    overall_total_o = 0
+    overall_total_p = 0
+
     for name in group_list: 
+        tmp_total_hours = 0
+        total_a = 0
+        total_i = 0
+        total_o = 0
+        total_p = 0
+
         if Entry.objects.filter(username=name, date=date):
-            t = Entry.objects.filter(username=name, date=date)
+            users_entries = Entry.objects.filter(username=name, date=date)
+            for entry in users_entries:
+                hours = entry.hours
+                tmp_total_hours += hours
+                if entry.category == a:
+                    total_a += hours
+                if entry.category == i:
+                    total_i += hours
+                if entry.category == o:
+                    total_o += hours
+                if entry.category == p:
+                    total_p += hours
+
+            overall_total_a += total_a
+            overall_total_i += total_i
+            overall_total_o += total_o
+            overall_total_p += total_p
+
+            temp_totals = {'A': str(total_a), 'I': str(total_i), 'O':str(total_o), 'P':str(total_p)}
+            total_hours_by_user[name] = temp_totals
+
             users_with_entries.append(str(name.username))
-            entries[name] = t
+            entries[name] = users_entries
+
         else:
-            t = ['No Entry']
             users_with_no_entries.append(str(name.username))
-        #entries.append(Entry.objects.filter(username=name.id, date=date))
-        if not t:
-            t = ['No Entry']
-    #FIXME MAKE THIS A DICT ASSOCIATED WITH EACH USERNAME
-    #FIXME why isn't this getting xiolan's entries??
-    context = {'entries': entries, 'previous':previous, 'next':next, 'date':date, 'users_with_no_entries':users_with_no_entries}
+
+    total_hours = {'A': str(overall_total_a), 'I': str(overall_total_i), 'O':str(overall_total_o), 'P':str(overall_total_p)}
+
+    #Now we need to get the totals
+
+
+    #totals = {}
+    #for tmp_name in users_with_entries:
+    #    temp_total = 0
+    #    x = entries[tmp_name]
+    #    for y in x:
+    #       temp_total += y.hours 
+
+
+    context = {'entries': entries, 'previous':previous, 'next':next, 'total_hours_by_user':total_hours_by_user, 'total_hours': total_hours, 'date':date, 'users_with_no_entries':users_with_no_entries}
     return render(request, 'timetracker/daily_manager_report.html', context)
 
             
