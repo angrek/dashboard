2076e02539aed058f0e62794ce31377dcd811f60
Boomer Rehfield <angrek@gmail.com>
2015-05-15 17:46:37 -0400

    Final? Working copy

--- y_aix_all_network_connections.py
+++ y_aix_all_network_connections.py
@@ -1,10 +1,9 @@
 #!/home/wrehfiel/ENV/bin/python2.7
 #########################################################################
 #
-# This is to pull data from Django and drop it into Excel for
-# capacity planning
+# EMC AIX server information report. This is for all network connections
 #
-# Boomer Rehfield - 10/22/2014
+# Boomer Rehfield - 5/7/2015
 #
 #########################################################################
 
@@ -18,6 +17,8 @@ from openpyxl.styles import Style, PatternFill, Border, Side, Alignment, Protect
 from django.utils import timezone
 from server.models import AIXServer, LinuxServer, Power7Inventory, Storage
 
+import utilities
+
 #these are need in django 1.7 and needed vs the django settings command
 import django
 from dashboard import settings
@@ -26,259 +27,279 @@ django.setup()
 
 s = Style(font=Font(name='Calibri', size=11, bold=True))
 
+#need to put the timestamp in the filename
+now = timezone.now()
+timestamp = now.strftime('%m-%d-%Y')
+filename = 'all_network_conections' + timestamp + '.xlsx'
 
 
 wb = Workbook()
 ws1 = wb.active
 ws1.title = 'AIX'
-ws2 = wb.create_sheet()
-ws2.title = 'Linux'
 
 
 #create le pretty headers
-ws1['A1'] = 'Host Name'
-ws1['B1'] = 'OS'
-ws1['C1'] = 'Physical/VM'
-ws1['D1'] = 'IP'
-ws1['E1'] = 'Mem(MB)'
-ws1['F1'] = 'Database Name'
-ws1['G1'] = 'Storage'
-ws1['H1'] = 'CPU Cores'
+ws1['A1'] = 'local_ip_addr'
+ws1['B1'] = 'local_port'
+ws1['C1'] = 'remote_ip_addr'
+ws1['D1'] = 'remote_port'
+ws1['E1'] = 'protocol'
+ws1['F1'] = 'state'
+ws1['G1'] = 'uid'
+ws1['H1'] = 'pid'
+ws1['I1'] = 'cmd'
 
-ws2['A1'] = 'Host Name'
-ws2['B1'] = 'OS'
-ws2['C1'] = 'Physical/VM'
-ws2['D1'] = 'IP'
-ws2['E1'] = 'Mem(MB)'
-ws2['F1'] = 'Database Name'
-ws2['G1'] = 'Storage'
-ws2['H1'] = 'CPU Cores'
 
 ws1.column_dimensions["A"].width = 20
-ws1.column_dimensions["B"].width = 10
+ws1.column_dimensions["B"].width = 16
 ws1.column_dimensions["C"].width = 20
+ws1.column_dimensions["D"].width = 16
+ws1.column_dimensions["E"].width = 20
 ws1.column_dimensions["F"].width = 20
-ws1.column_dimensions["H"].width = 12
+ws1.column_dimensions["G"].width = 20
+ws1.column_dimensions["H"].width = 20
+ws1.column_dimensions["I"].width = 150
 
-ws2.column_dimensions["A"].width = 20
-ws2.column_dimensions["B"].width = 10
-ws2.column_dimensions["C"].width = 20
-ws2.column_dimensions["F"].width = 20
-ws2.column_dimensions["H"].width = 12
-
-list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 
 #to hell with writing all this out because the row dimensions are working...
 for letter in list:
     cell = letter + '1'
     c = ws1[cell]
-    f = ws2[cell]
     c.style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    f.style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
 
 d = ws1.row_dimensions[1].height = 20
-g = ws2.row_dimensions[1].height = 20
 
 def get_server_data():
-#starting line = 3
-    line = 3
+    line = 1
     counter = 0
-    server_list = AIXServer.objects.filter(active=True, decommissioned=False)
+    server_list = AIXServer.objects.filter(active=True, exception=False, decommissioned=False).exclude(name__contains='sas').exclude(name__contains='spds')
 
     for server in server_list:
+        print server.name
 
-        counter = counter + 1
+        if utilities.ping(server):
+            
+            print 'ping good'
+            client = SSHClient()
+            if utilities.ssh(server, client):
+                
+                print 'ssh good'
 
-        t = AIXServer.objects.filter(name=server, active=True)
+                counter = counter + 1
 
-        try:
-            r = Power7Inventory.objects.get(name=server)
-        except:
-            pass
+                command = "netstat -An"
+                stdin, stdout, stderr = client.exec_command(command)
 
-        try:
-            p = Storage.objects.get(name=server)
-        except:
-            pass
-
-        print str(counter) + ',' + str(server) + ',AIX,VM,' + server.ip_address.rstrip() + ',' + str(r.curr_mem) + ',,' + str(p.size) + ',' + str(r.curr_procs)
-
-        cell = 'A' + str(line)
-        ws1[cell] = str(server)
-
-        cell = 'B' + str(line)
-        ws1[cell] = 'AIX'
-
-        cell = 'C' + str(line)
-        ws1[cell] = 'VM'
-
-        cell = 'D' + str(line)
-        ws1[cell] = server.ip_address.rstrip()
-
-        cell = 'E' + str(line)
-        ws1[cell] = r.curr_mem
-
-        cell = 'F' + str(line)
-        ws1[cell] = ''
-
-        cell = 'G' + str(line)
-        ws1[cell] = p.size
-
-        cell = 'H' + str(line)
-        ws1[cell] = r.curr_procs
+                output = stdout.readlines()
 
 
+                for row in output:
+                    line = line + 1
+                    if re.search('Active UNIX', row):
+                        line = line - 1
+                        break
+                    if re.search('Active', row):
+                        line = line - 1
+                        continue
+                    if re.search('PCB', row):
+                        line = line - 1
+                        continue
 
-        line += 1
-        aix_line = line
+                    if row.rstrip():
+                        print '>' + row.rstrip()
+                        row = row.split()
+                        protocol = row[1]
+                        print 'Protocol:' + protocol
+
+                        #these are giving issues for now
+                        if protocol == 'tcp6':
+                            line = line - 1
+                            continue
+                        if protocol == 'udp6':
+                            line = line - 1
+                            continue
+                        local_address = row[4]
+                        if local_address == '*.*':
+                            local_port = ''
+                        else:
+                            local_port = local_address.split('.')[4]
+                            x = local_address.split('.')
+                            local_address = x[0] + '.' + x[1] + '.' + x[2] + '.' + x[3]
+                        print 'Local Address: ' + local_address
+                        print 'Local Port:' + local_port
+
+                        remote_address = row[5]
+                        if remote_address == '*.*':
+                            remote_port = ''
+                        else:
+                            remote_port = remote_address.split('.')[4]
+                            x = remote_address.split('.')
+                            remote_address = x[0] + '.' + x[1] + '.' + x[2] + '.' + x[3]
+                        print 'Remote Address: ' + remote_address
+                        print 'Remote Port: ' + remote_port
+
+                        try:
+                            state = row[6]
+                            print 'State:' + state
+                        except:
+                            state = ''
+                            print 'State not shown'
+
+                        socket = row[0]
+                        command = "rmsock " + socket + " tcpcb"
+                        stdin, stdout, stderr = client.exec_command(command)
+                        try:
+                            pid = stdout.readlines()[0]
+                        except:
+                            pid = ''
+
+                        if re.search('proccess', pid):
+                            pid = pid.split()[8]
+                        else:
+                            pid = ''
+                        print pid
+
+                        command = "ps -ef | grep " + pid + " | grep -v grep"
+                        stdin, stdout, stderr = client.exec_command(command)
+                        try:
+                            out = stdout.readlines()[0].split()[0]
+                        except:
+                            out = ''
+
+                        print out
+                        uid = ''
+                        if out:
+                            command = "id -u " + out
+                            stdin, stdout, stderr = client.exec_command(command)
+                            try:
+                                uid = stdout.readlines()[0].rstrip()
+                            except:
+                                uid = 0
+                        print uid
+
+                        command = "ps -o args -p " + pid
+                        stdin, stdout, stderr = client.exec_command(command)
+                        try:
+                            cmd = stdout.readlines()[1].rstrip()
+                        except:
+                            cmd = ''
+
+                        print cmd
+
+                        cell = 'A' + str(line)
+                        ws1[cell] = local_address
+
+                        cell = 'B' + str(line)
+                        ws1[cell] = local_port
+
+                        cell = 'C' + str(line)
+                        ws1[cell] = remote_address
+
+                        cell = 'D' + str(line)
+                        ws1[cell] = remote_port
+
+                        cell = 'E' + str(line)
+                        ws1[cell] = protocol
+
+                        cell = 'F' + str(line)
+                        ws1[cell] = state
+
+                        cell = 'G' + str(line)
+                        ws1[cell] = uid
+
+                        cell = 'H' + str(line)
+                        ws1[cell] = pid
+
+                        cell = 'I' + str(line)
+                        ws1[cell] = cmd
 
 
-    #################STARTING LINUX SECTION############################
-    line = 3
-    counter = 0
-    linux_server_list = LinuxServer.objects.all(decommissioned=False)
+        
 
-    for server in linux_server_list:
-
-        counter = counter + 1
-
-        t = LinuxServer.objects.get(name=server)
-        if t.active == False:
-            #we don't care about inactive servers for capacity planning
-            continue
-
-        #try:
-        #    r = Power7Inventory.objects.get(name=server)
-        #except:
-        #    pass
-        #
-        #try:
-        #    p = Storage.objects.get(name=server)
-        #except:
-        #    pass
-
-        print str(counter) + ',' + str(server) + ',Linux,VM,' + t.ip_address.rstrip() + ',' + str(t.memory) + ',,' + ' n/a' + ',' + str(t.cpu)
-
-        cell = 'A' + str(line)
-        ws2[cell] = str(server)
-
-        cell = 'B' + str(line)
-        ws2[cell] = 'Linux'
-
-        cell = 'C' + str(line)
-        ws2[cell] = 'VM'
-
-        cell = 'D' + str(line)
-        ws2[cell] = t.ip_address.rstrip()
-
-        cell = 'E' + str(line)
-        ws2[cell] = t.memory
-
-        cell = 'F' + str(line)
-        ws2[cell] = ''
-
-        cell = 'G' + str(line)
-        ws2[cell] = ''
-
-        cell = 'H' + str(line)
-        ws2[cell] = t.cpu
-
-
-
-        #AIXServer.objects.filter(name=server).update(active=False, modified=timezone.now())
-        line += 1
 
 
 
     #total
-    cell = 'A' + str(line + 1)
-    aix_cell = 'A' +str(aix_line + 1)
-    ws1[aix_cell] = 'Total'
-    ws2[cell] = 'Total'
-    ws1[aix_cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    ws2[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    #cell = 'A' + str(line + 1)
+    #aix_cell = 'A' +str(aix_line + 1)
+    #ws1[aix_cell] = 'Total'
+    #ws2[cell] = 'Total'
+    #ws1[aix_cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    #ws2[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+#
+    #cell = 'E' + str(line + 1)
+    #aix_cell = 'E' + str(aix_line + 1)
+    #sum = "=SUM(E3:E" + str((line - 1)) + ")"
+    #aix_sum = "=SUM(E3:E" + str((aix_line -1)) + ")"
+    #print aix_sum
+    #print sum
+    #ws1[aix_cell] = aix_sum
+    #ws2[cell] = sum
+    #ws1[aix_cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    #ws2[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+#
+    #cell = 'G' + str(line + 1)
+    #aix_cell = 'G' + str(aix_line + 1)
+    #sum = "=SUM(G3:G" + str((line - 1)) + ")"
+    #aix_sum = "=SUM(G3:G" + str((aix_line - 1)) + ")"
+    #print aix_cell
+    #print aix_sum
+    #ws1[aix_cell] = aix_sum
+    #ws2[cell] = sum
+    #ws1[aix_cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    #ws2[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
 
-    cell = 'E' + str(line + 1)
-    aix_cell = 'E' + str(aix_line + 1)
-    sum = "=SUM(E3:E" + str((line - 1)) + ")"
-    aix_sum = "=SUM(E3:E" + str((aix_line -1)) + ")"
-    print aix_sum
-    print sum
-    ws1[aix_cell] = aix_sum
-    ws2[cell] = sum
-    ws1[aix_cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    ws2[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-
-    cell = 'G' + str(line + 1)
-    aix_cell = 'G' + str(aix_line + 1)
-    sum = "=SUM(G3:G" + str((line - 1)) + ")"
-    aix_sum = "=SUM(G3:G" + str((aix_line - 1)) + ")"
-    print aix_cell
-    print aix_sum
-    ws1[aix_cell] = aix_sum
-    ws2[cell] = sum
-    ws1[aix_cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    ws2[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-
-    cell = 'H' + str(line + 1)
-    aix_cell = 'H' + str(aix_line + 1)
-    sum = "=SUM(H3:H" + str((line - 1)) + ")"
-    aix_sum = "=SUM(H3:H" + str((aix_line - 1)) + ")"
-    ws1[aix_cell] = aix_sum
-    ws2[cell] = sum
-    ws1[aix_cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    ws2[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    #cell = 'H' + str(line + 1)
+    #aix_cell = 'H' + str(aix_line + 1)
+    #sum = "=SUM(H3:H" + str((line - 1)) + ")"
+    #aix_sum = "=SUM(H3:H" + str((aix_line - 1)) + ")"
+    #ws1[aix_cell] = aix_sum
+    #ws2[cell] = sum
+    #ws1[aix_cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    #ws2[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
 
 #create le pretty headers
-    cell = 'A' + str(aix_line + 3)
-    ws1[cell] = 'Host Name'
+    cell = 'A1'
+    ws1[cell] = 'local_ip_addr'
     ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    cell = 'B' + str(aix_line + 3)
-    ws1[cell] = 'OS'
+    cell = 'B1'
+    ws1[cell] = 'local_port'
     ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    cell = 'C' + str(aix_line + 3)
-    ws1[cell] = 'Physical/VM'
+    cell = 'C1'
+    ws1[cell] = 'remote_ip_addr'
     ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    cell = 'D' + str(aix_line + 3)
-    ws1[cell] = 'IP'
+    cell = 'D1'
+    ws1[cell] = 'remote_port'
     ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    cell = 'E' + str(aix_line + 3)
-    ws1[cell] = 'Mem(MB)'
+    cell = 'E1'
+    ws1[cell] = 'protocol'
     ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    cell = 'F' + str(aix_line + 3)
-    ws1[cell] = 'Database Name'
+    cell = 'F1'
+    ws1[cell] = 'state'
     ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    cell = 'G' + str(aix_line + 3)
-    ws1[cell] = 'Storage'
+    cell = 'G1'
+    ws1[cell] = 'uid'
     ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    cell = 'H' + str(aix_line + 3)
-    ws1[cell] = 'CPU Cores'
+    cell = 'H1'
+    ws1[cell] = 'pid'
+    ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    cell = 'I1'
+    ws1[cell] = 'cmd'
     ws1[cell].style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
 
-    #ws2['A1'] = 'Host Name'
-    #ws2['B1'] = 'OS'
-    #ws2['C1'] = 'Physical/VM'
-    #ws2['D1'] = 'IP'
-    #ws2['E1'] = 'Mem(MB)'
-    #ws2['F1'] = 'Database Name'
-    #ws2['G1'] = 'Storage'
-    #ws2['H1'] = 'CPU Cores'
 
-    #need to put the timestamp in the filename
-    now = timezone.now()
-    timestamp = now.strftime('%m-%d-%Y')
-    filename = 'Unix_Configuration_Report' + timestamp + '.xlsx'
-    wb.save(filename)
 
 
     #c and f are just arbitrary vars right now for page one and two :\
     c.style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
-    f.style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
+    #f.style = Style(font=Font(name='Arial', size=11, bold=True, vertAlign=None, color='FF000000'))
 
     command = 'echo "Unix Configuration Report" | mutt -a "' + filename + '" -s "Unix Configuration Report" -- boomer@wellcare.com'
     os.system(command)
 
 
+    wb.save(filename)
 
 
 
