9791ed10ff58064b1cb61a73e3eb0df877602b00
Boomer Rehfield <angrek@gmail.com>
2015-05-20 08:48:02 -0400

    Just copying it out into two different scripts

---
+++ x_cent_install_aix.py
@@ -0,0 +1,121 @@
+#!/home/wrehfiel/ENV/bin/python2.7
+#########################################################################
+#
+# Script to upgrade to the new centrify patch
+#
+# Boomer Rehfield - 5/19/2014
+#
+#########################################################################
+
+import os, re, time
+from ssh import SSHClient
+from django.utils import timezone
+#these are need in django 1.7 and needed vs the django settings command
+import django
+from dashboard import settings
+from server.models import AIXServer
+import utilities
+django.setup()
+
+
+def update_server():
+
+    server_list = AIXServer.objects.filter(name='t1sandbox', zone=1, active=True, exception=False, decommissioned=False).exclude(centrify='5.2.2-192')
+
+    counter = 0
+
+    for server in server_list:
+
+        print '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+        print '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+        print '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+        print server.name
+
+        if utilities.ping(server):
+
+            client = SSHClient()
+            if utilities.ssh(server, client):
+                print "Current centrify version:"
+                print server.centrify
+
+                if server.centrify != '5.2.2-192':
+
+                    print 'Creating directory /unix'
+                    command = 'dzdo mkdir /unix'
+                    stdin, stdout, stderr = client.exec_command(command)
+                    x = stdout.readlines()
+                    y = stderr.readlines()
+                    print x
+                    for line in x:
+                        print line
+                    for line in y:
+                        print line
+
+
+                    print 'Mounting naswin1 /unix'
+                    command = 'dzdo mount -o nolock naswin1:/unix /unix'
+                    stdin, stdout, stderr = client.exec_command(command)
+                    x = stdout.readlines()
+                    y = stderr.readlines()
+                    print '2'
+                    for line in x:
+                        print line
+                    for line in y:
+                        print line
+
+                    print 'Installing centrify'
+                    command = 'dzdo rpm -Uvh /unix/software/Centrify/Centrify-Suite-2015-agents-DM/SP1/tmp/centrifydc-5.2.2-rhel3-x86_64.rpm'
+                    stdin, stdout, stderr = client.exec_command(command)
+                    x = stdout.readlines()
+                    y = stderr.readlines()
+                    print '3'
+                    for line in x:
+                        print line
+                    for line in y:
+                        print line
+
+                    command = 'dzdo service centrifydc restart;dzdo service sshd restart;dzdo adflush -f'
+                    stdin, stdout, stderr = client.exec_command(command)
+                    x = stdout.readlines()
+                    for line in x:
+                        print line
+
+                    command = 'adinfo -v'
+                    stdin, stdout, stderr = client.exec_command(command)
+                    x = stdout.readlines()
+                    for line in x:
+                        print line
+
+
+            else:
+                print "No ssh"
+        else:
+            print "No ping"
+
+                #t = stdout.readlines()
+                #for line in t:
+                #    print line.rstrip()
+#
+ #               print '---------------------------'
+
+                #command = 'cat /etc/rsyslog.conf'
+                #stdin, stdout, stderr = client.exec_command(command)
+
+                #try changing chkconfig
+                #command = 'chkconfig --list | grep rsyslog'
+                #stdin, stdout, stderr = client.exec_command(command)
+                #t = stdout.readlines()[0]
+                #print t
+
+
+            
+
+#start execution
+if __name__ == '__main__':
+    print "Checking and installing rsyslog."
+    starting_time = timezone.now()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
+    update_server()
+    elapsed_time = timezone.now() - starting_time 
+    print "Elapsed time: " + str(elapsed_time)
+
