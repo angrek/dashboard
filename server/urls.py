7599f9bd2b7a6fbfa9a3f810c0696c08649a1d4f
Boomer Rehfield <angrek@gmail.com>
2015-09-28 16:01:42 -0400

    Added datatables test for new type of front end table testing

--- server/urls.py
+++ server/urls.py
@@ -7,8 +7,11 @@ from server import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^jquerytest/$', views.jquerytest, name='jquerytest'),
+
     url(r'^stacks/(?P<os>(.*)+)/(?P<zone>(.*)+)$', views.stacks, name='stacks'),
     url(r'^stacks/(?P<os>(.*)+)/(?P<zone>(.*)+)/$', views.stacks, name='stacks'),
+
+
     url(r'^frames$', views.frames, name='frames'),
     url(r'^frames/$', views.frames, name='frames'),
     url(r'^wpars$', views.wpars, name='wpars'),
@@ -39,11 +42,10 @@ urlpatterns = patterns('',
     url(r'^column_basic_proc_pools/(?P<frame>(.*)+)/(?P<pool_name>(.*)+)/(?P<period>(.*)+)/(?P<time_range>(.*)+)$', views.column_basic_proc_pools, name='column_basic_proc_pools'),
     url(r'^column_basic_proc_pools/(?P<frame>(.*)+)/(?P<pool_name>(.*)+)/(?P<period>(.*)+)/(?P<time_range>(.*)+)/$', views.column_basic_proc_pools, name='column_basic_proc_pools'),
 
+    #treemap view of the AIX affinity values
     url(r'^treemap/$', views.treemap, name='treemap'),
     url(r'^treemap$', views.treemap, name='treemap'),
 
-    #Can I do something like this? This is what I need....
-    #url(r'^3d_pie/<historical_today>/<service>/$', views.pie_ssh, name='pie_ssh'),
 
     url(r'^(?P<aixserver_name>(.*)+)/$', views.detail, name='detail'),
 
@@ -54,5 +56,11 @@ urlpatterns = patterns('',
     #Git statistics for ansible-dev
     url(r'^git_stats_ansible_dev$', views.git_stats_ansible_dev, name='git_stats_ansible_dev'),
     url(r'^git_stats_ansible_dev/$', views.git_stats_ansible_dev, name='git_stats_ansible_dev'),
+
+    #Testing area
+
+    #Testing dataTables with django
+    url(r'^aix_server_table$', views.aix_server_table, name='aix_server_table'),
+    url(r'^aix_server_table/$', views.aix_server_table, name='aix_server_table'),
 
 )
