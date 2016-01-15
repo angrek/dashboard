81cee301b0cebd1420400a0e29de881f919e2f1e
Boomer Rehfield <angrek@gmail.com>
2016-01-15 11:37:26 -0500

    Testing - realized it isn't logging to the admin, duh

--- linux_populate.pl
+++ linux_populate.pl
@@ -123,7 +123,7 @@ foreach my $host (@$host_views) {
         #    print "YESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS";
         #}
         #if ($vm->guest->guestFamily eq 'llinuxGuest') {
-
+            print "MADE IT HERE!";
             $tmp = ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
             $timestamp = sprintf ("%04d-%02d-%02d %02d:%02d:%02d",
                                     $year+1900,$mon+1,$mday,$hour,$min,$sec);
