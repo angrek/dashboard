d7872910d374880d851972f654c571c756768a42
Boomer Rehfield <angrek@gmail.com>
2016-02-26 10:23:16 -0500

    initial checkin

---
+++ linux_glibc.py
@@ -0,0 +1,62 @@
+#!/home/wrehfiel/ENV/bin/python2.7
+#########################################################################
+#
+# Script to retrieve Linux glibc versions on the servers and drop them into Django dashboard
+#
+# Boomer Rehfield - 2/24/2016
+#
+#########################################################################
+
+import os
+import re
+from ssh import SSHClient
+import paramiko
+from django.utils import timezone
+#these are need in django 1.7 and needed vs the django settings command
+import django
+from dashboard import settings
+import utilities
+import utilities
+django.setup()
+
+
+def update_server():
+
+    server_list = LinuxServer.objects.filter(decommissioned=False)
+
+    for server in server_list:
+            
+        if utilities.ping(server):
+
+            client = SSHClient()
+            if utilities.ssh(server, client):
+
+                stdin, stdout, stderr = client.exec_command('dzdo rpm -qa | grep glibc | grep -v devel | grep -v common | grep -v compat | grep -v static | grep -v headers | uniq | tail -n 1')
+                #this is going to pull 4 different parts of ssl, we just need the base
+                glibc = stdout.readlines()[0]
+                #glibc = str(rows[0]).rstrip().rstrip()
+                print glibc
+                #
+
+                #cut off the beginning and end, not really needed and saves space on the spreadsheet view.
+                glibc = re.sub('glibc-', '', glibc)
+                glibc = re.sub('.x86_64', '', glibc)
+                glibc = re.sub('.i686', '', glibc)
+
+                print glibc
+                #if existing value is the same, don't update
+                if str(glibc) != str(server.glibc):
+                    utilities.log_change(server, 'glibc', str(server.glibc), str(glibc))
+                    LinuxServer.objects.filter(name=server, exception=False, active=True).update(glibc=glibc, modified=timezone.now())
+
+
+
+#start execution
+if __name__ == '__main__':
+    print "Checking glibc versions..."
+    start_time = timezone.now()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
+    from server.models import LinuxServer
+    update_server()
+    elapsed_time = timezone.now() - start_time
+    print "Elapsed time: " + str(elapsed_time)
