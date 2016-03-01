ab90d60ad7f58de0e11b63ddfec3d3319a99b750
Boomer Rehfield <angrek@gmail.com>
2016-03-01 13:25:02 -0500

    Added parallel processing

--- aix_bash.py
+++ aix_bash.py
@@ -15,37 +15,35 @@ import django
 from dashboard import settings
 from server.models import AIXServer
 import utilities
-
+from multiprocessing import Pool
 django.setup()
 
 
-def update_server():
+def update_server(server):
 
-    server_list = AIXServer.objects.filter(decommissioned=False)
 
-    for server in server_list:
 
-        if utilities.ping(server):
+    if utilities.ping(server):
+        
+        client = SSHClient()
+        if utilities.ssh(server, client):
+            print server.name                
+            command = 'rpm -qa | grep bash |grep -v doc'
+            stdin, stdout, stderr = client.exec_command(command)
+            try:
+                bash_version = stdout.readlines()[0].rstrip()
+            except:
+                print "Problem getting bash version"
+                continue
+            stdin.close()
+            stderr.close()
             
-            client = SSHClient()
-            if utilities.ssh(server, client):
-                print server.name                
-                command = 'rpm -qa | grep bash |grep -v doc'
-                stdin, stdout, stderr = client.exec_command(command)
-                try:
-                    bash_version = stdout.readlines()[0].rstrip()
-                except:
-                    print "Problem getting bash version"
-                    continue
-                stdin.close()
-                stderr.close()
-                
-                #check existing value, if it exists, don't update
-                if str(bash_version) != str(server.bash):
-                    utilities.log_change(server, 'bash', str(server.bash), str(bash_version))
+            #check existing value, if it exists, don't update
+            if str(bash_version) != str(server.bash):
+                utilities.log_change(server, 'bash', str(server.bash), str(bash_version))
 
-                    AIXServer.objects.filter(name=server).update(bash=bash_version, modified=timezone.now())
-                client.close()
+                AIXServer.objects.filter(name=server).update(bash=bash_version, modified=timezone.now())
+            client.close()
 
 
 
@@ -54,7 +52,12 @@ if __name__ == '__main__':
     print "Checking Bash versions..."
     starting_time = timezone.now()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
-    update_server()
+
+    
+    server_list = AIXServer.objects.filter(decommissioned=False)
+    pool = Pool(20)
+    pool.map(update_server, server_list)
+
     elapsed_time = timezone.now() - starting_time 
     print "Elapsed time: " + str(elapsed_time)
 
