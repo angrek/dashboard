ff3998432a5e3587cf9d0850916b507eb4b86f54
Boomer Rehfield <angrek@gmail.com>
2016-03-01 13:42:18 -0500

    Added parallel processing

--- linux_oslevel.py
+++ linux_oslevel.py
@@ -16,78 +16,80 @@ import django
 from dashboard import settings
 from server.models import LinuxServer
 import utilities
+from multiprocessing import Pool
 django.setup()
 
 
-def update_server():
+def update_server(server):
 
-    server_list = LinuxServer.objects.filter(decommissioned=False)
+    if utilities.ping(server):
 
-    for server in server_list:
-        print server
-        if utilities.ping(server):
+        client = SSHClient()
+        if utilities.ssh(server, client):
 
-            client = SSHClient()
-            if utilities.ssh(server, client):
+            print server.name
 
-                print server.name
+            command = 'lsb_release -a | grep Distributor'
+            stdin, stdout, stderr = client.exec_command(command)
 
-                command = 'lsb_release -a | grep Distributor'
-                stdin, stdout, stderr = client.exec_command(command)
+            #need rstrip() because there are extra characters at the end
+            #FIXME - dinfhdp09 doesn't have lsb_release installed?????
+            try:
+                os = stdout.readlines()[0].rstrip()
+            except:
+                continue
 
-                #need rstrip() because there are extra characters at the end
-                #FIXME - dinfhdp09 doesn't have lsb_release installed?????
-                try:
-                    os = stdout.readlines()[0].rstrip()
-                except:
-                    continue
-                os = re.sub('Distributor ID:', '', os)
-                os = re.sub('\s*', '', os)
+            os = re.sub('Distributor ID:', '', os)
+            os = re.sub('\s*', '', os)
 
 
-                if os == 'RedHatEnterpriseServer':
-                    os = 'RHEL'
-                else:
-                    os = 'Unknown'
-                print os
+            if os == 'RedHatEnterpriseServer':
+                os = 'RHEL'
+            else:
+                os = 'Unknown'
+            print os
 
-                command = 'lsb_release -a | grep Release'
-                stdin, stdout, stderr = client.exec_command(command)
+            command = 'lsb_release -a | grep Release'
+            stdin, stdout, stderr = client.exec_command(command)
 
-                oslevel = stdout.readlines()[0].rstrip()
-                oslevel = re.sub('Release:', '', oslevel)
-                oslevel = re.sub('\s*', '', oslevel)
-                print oslevel
+            oslevel = stdout.readlines()[0].rstrip()
+            oslevel = re.sub('Release:', '', oslevel)
+            oslevel = re.sub('\s*', '', oslevel)
+            print oslevel
 
-                command = 'uname -r'
-                stdin, stdout, stderr = client.exec_command(command)
-                kernel = stdout.readlines()[0].rstrip()
-                print kernel
-                
-                command = 'dzdo rpm -qa --last | grep kernel'
-                stdin, stdout, stderr = client.exec_command(command)
-                kernel_date = stdout.readlines()[0].rstrip()
-                print kernel_date
-                
+            command = 'uname -r'
+            stdin, stdout, stderr = client.exec_command(command)
+            kernel = stdout.readlines()[0].rstrip()
+            print kernel
+            
+            command = 'dzdo rpm -qa --last | grep kernel'
+            stdin, stdout, stderr = client.exec_command(command)
+            kernel_date = stdout.readlines()[0].rstrip()
+            print kernel_date
+            
 
 
-                #check existing value, if it exists, don't update
-                if str(os) != str(server.os):
-                    utilities.log_change(server, 'OS', str(server.os), str(os))
-                    LinuxServer.objects.filter(name=server).update(os=os, modified=timezone.now())
-                if str(oslevel) != str(server.os_level):
-                    utilities.log_change(server, 'OS Level', str(server.os_level), str(oslevel))
-                    LinuxServer.objects.filter(name=server).update(os_level=oslevel, modified=timezone.now())
+            #check existing value, if it exists, don't update
+            if str(os) != str(server.os):
+                utilities.log_change(server, 'OS', str(server.os), str(os))
+                LinuxServer.objects.filter(name=server).update(os=os, modified=timezone.now())
+            if str(oslevel) != str(server.os_level):
+                utilities.log_change(server, 'OS Level', str(server.os_level), str(oslevel))
+                LinuxServer.objects.filter(name=server).update(os_level=oslevel, modified=timezone.now())
 
-                if str(kernel) != str(server.kernel):
-                    utilities.log_change(server, 'Kernel', str(server.kernel), str(kernel))
-                    LinuxServer.objects.filter(name=server).update(kernel=kernel, modified=timezone.now())
+            if str(kernel) != str(server.kernel):
+                utilities.log_change(server, 'Kernel', str(server.kernel), str(kernel))
+                LinuxServer.objects.filter(name=server).update(kernel=kernel, modified=timezone.now())
 
 
 if __name__ == '__main__':
     print "Checking OS versions..."
     start_time = timezone.now()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
-    update_server()
+
+    server_list = LinuxServer.objects.filter(decommissioned=False)
+    pool = Pool(20)
+    pool.map(update_server, server_list)
+
     elapsed_time = timezone.now() - start_time
     print "Elapsed time: " + str(elapsed_time)
