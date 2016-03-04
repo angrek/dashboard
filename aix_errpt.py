f36ae65101b200e5553b169894a25e529a99f9c8
Boomer Rehfield <angrek@gmail.com>
2016-03-04 14:19:29 -0500

    Errpts were turned off for some reason??

--- aix_errpt.py
+++ aix_errpt.py
@@ -16,43 +16,42 @@ import django
 from dashboard import settings
 from server.models import AIXServer, Errpt
 import utilities
+from multiprocessing import Pool
 django.setup()
 
 
 
-def update_server():
-    #right now we are just getting these for the VIO servers
-    server_list = AIXServer.objects.filter(name__contains='vio')
+def update_server(server):
 
-    for server in server_list:
+    if utilities.ping(server):
 
-        if utilities.ping(server):
+        client = SSHClient()
+        if utilities.ssh(server, client):
+            stdin, stdout, stderr = client.exec_command('errpt | tail -n 20"')
+            report = ''
 
-            client = SSHClient()
-            if utilities.ssh(server, client):
-                stdin, stdout, stderr = client.exec_command('errpt | tail -n 20"')
-                report = ''
-
-                #we have to do errpt differently due to the way it is handled by stdout
-                for line in stdout:
-                    report = report + str(line)
-                if report == '':
-                    report = "The errpt was empty."
-            
-                #we don't care about the old record and we'll just add another
-                Errpt.objects.get_or_create(name=server, report=report, modified=timezone.now())
-                #change_message = 'Updated errpt.'
-                #LogEntry.objects.create(action_time=timezone.now(), user_id=11, content_type_id=9, object_id=264, object_repr=server, action_flag=2, change_message=change_message)
+            #we have to do errpt differently due to the way it is handled by stdout
+            for line in stdout:
+                report = report + str(line)
+            if report == '':
+                report = "The errpt was empty."
+        
+            #we don't care about the old record and we'll just add another
+            Errpt.objects.get_or_create(name=server, report=report, modified=timezone.now())
+            #LogEntry.objects.create(action_time=timezone.now(), user_id=11, content_type_id=9, object_id=264, object_repr=server, action_flag=2, change_message=change_message)
 
 
 
 
-#start execution
 if __name__ == '__main__':
     print "Getting Errpts..."
     start_time = timezone.now()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
-    from server.models import AIXServer
-    update_server()
+
+    server_list = AIXServer.objects.filter(name__contains='vio')
+
+    pool = Pool(30)
+    pool.map(update_server, server_list)
+
     elapsed_time = timezone.now() - start_time
     print "Elapsed time: " + str(elapsed_time)
