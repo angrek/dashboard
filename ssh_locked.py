215d3e8c42a0cbfef16c7f3b26606b199b0946c1
Boomer Rehfield <angrek@gmail.com>
2016-04-29 15:13:39 -0400

    Added a script to unlock my account on aix boxes where something has hosed up

---
+++ ssh_locked.py
@@ -0,0 +1,42 @@
+#!/home/wrehfiel/ENV/bin/python2.7
+#########################################################################
+#
+# Script to unlock my account on AIX servers
+#  -usually happens on new builds
+#
+# Boomer Rehfield - 4/26/2016
+#
+#########################################################################
+
+import os
+from subprocess import *
+from ssh import SSHClient
+from django.utils import timezone
+#these are need in django 1.7 and needed vs the django settings command
+import django
+from dashboard import settings
+from server.models import AIXServer, LinuxServer
+import utilities
+
+django.setup()
+
+
+def update_server():
+
+    #server_list = AIXServer.objects.filter(name__contains='uts0')
+    server_list = AIXServer.objects.filter(decommissioned=False, active=True, exception=True)
+
+    for server in server_list:
+
+        command = 'dzdo ssh -q ' + server.name + ' /scripts/unlockuser.sh wrehfiel'
+        os.system(command)
+
+
+if __name__ == '__main__':
+    print "unlocking your account..."
+    starting_time = timezone.now()
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
+    update_server()
+    elapsed_time = timezone.now() - starting_time 
+    print "Elapsed time: " + str(elapsed_time)
+
