b7a50965b9432281b24a84bfc2d4b7666581fb25
Boomer Rehfield <angrek@gmail.com>
2016-06-24 14:57:21 -0400

    PEP 8 cleanup

--- centrify_testing.py
+++ centrify_testing.py
@@ -8,44 +8,40 @@
 #########################################################################
 
 import os
-from ssh import SSHClient
-from django.utils import timezone
-#these are need in django 1.7 and needed vs the django settings command
-import django
-from dashboard import settings
-from server.models import AIXServer, CentrifyUserCountAIX, CentrifyUserCountLinux
+# from ssh import SSHClient
 
-import utilities
+# these are need in django 1.7 and needed vs the django settings command
+import django
+from django.utils import timezone
+
+# from server.models import AIXServer, CentrifyUserCountAIX
+# from server.models import CentrifyUserCountLinux
 django.setup()
 
 
-
 def update_server():
-    #run_time=str(timezone.now())
-    #server_list = AIXServer.objects.filter(decommissioned=False, active=True, exception=False).exclude(name__contains='vio')
+
+    # run_time=str(timezone.now())
+    # server_list = AIXServer.objects.filter(decommissioned=False, active=True, exception=False).exclude(name__contains='vio')
+
+    # CentrifyUserCountAIX.objects.create(run_time=run_time, name=server, user_count=user_count)
+
+    # print server.name
+    # print user_count
+    # if it's the same version, we don't need to update the record
+    # if str(user_count) != str(server.centrify_user_count):
+
+    # CentrifyUserCountAIX.objects.create(run_time=run_time, name=server, user_count=user_count)
+
+    # utilities.log_change(server, 'Centrify user count', str(server.centrify_user_count), str(user_coutn))
+    pass
 
 
-    CentrifyUserCountAIX.objects.create(run_time=run_time, name=server, user_count=user_count)
-
-    print server.name
-    print user_count
-    #if it's the same version, we don't need to update the record
-    #if str(user_count) != str(server.centrify_user_count):
-
-    #CentrifyUserCountAIX.objects.create(run_time=run_time, name=server, user_count=user_count)
-
-    #utilities.log_change(server, 'Centrify user count', str(server.centrify_user_count), str(user_coutn))
-
-                            
-                        
-
-
-#start execution
 if __name__ == '__main__':
     print "Checking centrify version..."
     starting_time = timezone.now()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
-    from server.models import AIXServer
+
     update_server()
-    elapsed_time= timezone.now() - starting_time
+    elapsed_time = timezone.now() - starting_time
     print "Elapsed time: " + str(elapsed_time)
